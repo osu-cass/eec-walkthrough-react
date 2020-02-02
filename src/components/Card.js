325ef2d71f3a488b3c35c6a7cf24317b1a5920b6
dongrogers <dongrog@oregonstate.edu>
2020-02-01 22:30:02 -0800

    Refactored card container and card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,24 +2,67 @@ import React, { useState } from 'react'
 import Edit from './Edit'
 import BulletPoint from './BulletPoint'
 
-const Card = props => {
-  return (
-    <div
-      id={props.category}
-      className="my-3 p-3 bg-white card rounded shadow-sm"
-    >
-      <div
-        id="header"
-        className="d-flex justify-content-between border-bottom border-gray pb-2"
-      >
-        <h5>{props.category}</h5>
-        <Edit />
-      </div>
-      <div className="mt-2">
-				{props.children}
-      </div>
-    </div>
-  )
+class Card extends React.Component { 
+	state = {
+		tidbits: []
+	}
+
+	componentDidMount() {
+		fetch(`/cards/${this.props.id}`)
+				.then(res => res.json())
+				.then(tidbits => this.setState({tidbits: tidbits}));
+  }
+	
+	getChilds = (id) => {
+		var results = this.state.tidbits.reduce(function(result, tidbit) { //get tidbits whose parentid matches the parameter
+			if(tidbit.ParentID === id){
+				result.push(tidbit);
+			}
+			return result;
+		}, []);
+		return results.length ? results : false
+	}
+
+	recurseTidbits = (tidbit, icon, typeid, used) => {
+		let childs = this.getChilds(tidbit.TidbitID); //get all childs of this tidbit
+		if(!(used.includes(tidbit.TidbitID))){
+			used.push(tidbit.TidbitID)															//push used
+			if(childs){																							//if has child, recurse
+				return (
+					<BulletPoint icon={icon} text={tidbit.Text}>
+						{childs.map((child) => (this.recurseTidbits(child, icon, typeid, used)))}
+					</BulletPoint>
+				)
+			}
+			else{
+				return <BulletPoint icon={icon} text={tidbit.Text} /> //if no childs
+			}
+		}
+	}
+
+	render(){
+		return (
+			<div
+				id={this.props.category}
+				className={`my-3 p-3 bg-white card rounded shadow-sm ${this.props.checkFilter}`}
+			>
+				<div
+					id="header"
+					className="d-flex justify-content-between border-bottom border-gray pb-2"
+				>
+					<h5>{this.props.category}</h5>
+					<Edit />
+				</div>
+				<div className="mt-2">
+					{this.state.tidbits.map((tidbit) => {								//Loop through Tidbits of Type 
+							if(tidbit.TypeID === this.props.typeid){
+								return(this.recurseTidbits(tidbit, this.props.icon, this.props.typeid, this.props.used))
+							}})
+						}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Card
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -13,51 +13,20 @@ class CardContainer extends React.Component {
 		fetch(`/cards/types/${this.props.id}`)
 				.then(res => res.json())
 				.then(tidbitTypes => this.setState({tidbitTypes: tidbitTypes}));
-		fetch(`/cards/${this.props.id}`)
-				.then(res => res.json())
-				.then(tidbits => this.setState({tidbits: tidbits}));
   }
 
-	getChilds = (id) => {
-		var results = this.state.tidbits.reduce(function(result, tidbit) { //get tidbits whose parentid matches the parameter
-			if(tidbit.ParentID === id){
-				result.push(tidbit);
-			}
-			return result;
-		}, []);
-		return results.length ? results : false
-	}
-
-	recurseTidbits = (tidbit, icon, typeid, used) => {
-		let childs = this.getChilds(tidbit.TidbitID); //get all childs of this tidbit
-		if(!(used.includes(tidbit.TidbitID))){
-			used.push(tidbit.TidbitID)															//push used
-			if(childs){																							//if has child, recurse
-				return (
-					<BulletPoint icon={icon} text={tidbit.Text}>
-						{childs.map((child) => (this.recurseTidbits(child, icon, typeid, used)))}
-					</BulletPoint>
-				)
-			}
-			else{
-				return <BulletPoint icon={icon} text={tidbit.Text} /> //if no childs
-			}
-		}
-	}
-
 	generateCards = () => {
-		let used = [];
+		let used = []; //holds ids of all the used tidbits, prevents reprint
 		let Cards = this.state.tidbitTypes.map((type, i) => {				//Loop through Tidbit Types
 			return(
-				<div id={type.TypeID} className={this.checkFilter(type.TypeID)}>
-					<Card category={type.TypeName}>
-						{this.state.tidbits.map((tidbit) => {								//Loop through Tidbits of that Type
-							if(tidbit.TypeID === type.TypeID){
-								return(this.recurseTidbits(tidbit, type.Icon, type.TypeID, used))
-							}})
-						}
-					</Card>
-				</div>
+					<Card 
+						category={type.TypeName}
+						checkFilter={this.checkFilter(type.TypeID)}
+						id={this.props.id}
+						typeid={type.TypeID}
+						icon={type.Icon}
+						used={used}
+					/>
 			);
 		})
 		return Cards
