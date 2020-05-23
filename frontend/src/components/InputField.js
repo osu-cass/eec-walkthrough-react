d11cef55f7d2e425bd5a8ad6edcd0fdaff2d8a96
Kenny Luong <luongk@oregonstate.edu>
2020-05-23 10:42:24 -0700

    inserted styled changes, still untested

--- frontend/src/components/InputField.js
+++ frontend/src/components/InputField.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import FormControl from 'react-bootstrap/FormControl'
 
 class InputField extends React.Component {
 	state = {
@@ -8,9 +9,7 @@ class InputField extends React.Component {
 	render() {
 		return (
 			<Fragment>
-				<input
-					type="text"
-					className="form-control"
+				<FormControl
 					placeholder={this.props.placeholder}
 					value={this.props.value}
 					aria-label="Insert Username"
@@ -26,6 +25,7 @@ class InputField extends React.Component {
 					aria-describedby="basic-addon1"
 					onChange={(e) => this.props.handleInput(e, this.props.index)}
 					required
+				/>
 				 */}
 			</Fragment>
 		);
