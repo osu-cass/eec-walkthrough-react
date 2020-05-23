c5c8a245f497a34d61b630b9bb9e5925baea8d76
dongrog <dongrog@oregonstate.edu>
2020-05-23 16:46:03 -0700

    Added reset filter function

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import './Filter.css'
+import PropTypes from 'prop-types';
 
 class Filter extends React.Component {
-  state = {
-    active: false,
-  }
-
   handleClick = () => {
-    this.setState({ active: !this.state.active })
+    this.props.setActivity(this.props.idx);
     this.props.handleFilter(this.props.id, this.props.headerIndex)
   }
 
@@ -15,12 +12,22 @@ class Filter extends React.Component {
     return (
       <i
         className={`fas fa-${this.props.icon} ${
-          this.state.active ? 'fa-disabled' : ''
+          this.props.active ? '' : 'fa-disabled'
           } text-dark mr-3`}
         onClick={this.handleClick}
       ></i>
     )
   }
 }
+
+Filter.propTypes = {
+  idx: PropTypes.number,
+  id: PropTypes.number,
+  headerIndex: PropTypes.number,
+  icon: PropTypes.string,
+  setActivity: PropTypes.func,
+  handleFilter: PropTypes.func,
+  active: PropTypes.bool
+};
 
 export default Filter
