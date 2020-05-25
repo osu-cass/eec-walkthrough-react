68ac59be1eec53a65f930b5eb154d7e8c27390e7
silverware13 <thomasza@oregonstate.edu>
2020-05-25 13:48:39 -0700

    Add GET users/search/{text}/{role}/{cursorPrimary}/{cursorSecondary} endpoint

--- backend/models/users.js
+++ backend/models/users.js
@@ -181,3 +181,105 @@ async function updateUser(userId, userName, password, firstName, lastName, email
 
 }
 exports.updateUser = updateUser;
+
+
+// gets users who match the search query
+async function searchUsers(text, role, cursor) {
+  try {
+
+    const RESULTS_PER_PAGE = 25;
+    const sqlArray = [];
+    let users;
+    const nextCursor = {
+      primary: "null",
+      secondary: "null"
+    };
+
+    // initial sql query
+    let sql =
+      "SELECT userId, userName, firstName, lastName, email, role FROM Users ";
+
+    // only use the cursor if it isn't the initial search request
+    if (cursor.primary === "null") {
+      sql += "WHERE TRUE ";
+    } else {
+
+      // We set our primary cursor to the username name as it is the value
+      // that we are sorting by.
+      //
+      // Instances where the primary cursor value could have duplicate values
+      // are handled by also sorting by user ID.
+
+      sql += "WHERE userName >= ? AND " +
+        "userName > ? OR userId >= ? )) ";
+      sqlArray.push(cursor.primary);
+      sqlArray.push(cursor.primary);
+      sqlArray.push(cursor.secondary);
+
+    }
+
+    // get the text we are searching for
+    if (text !== "*") {
+      sql += "AND userName LIKE CONCAT('%', ?, '%') " +
+      "OR (CONCAT(firstName , ' ' , lastName) LIKE CONCAT('%', ?, '%') " +
+      "OR email LIKE CONCAT('%', ?, '%') " +
+      "OR userId LIKE CONCAT('%', ?, '%')) ";
+      sqlArray.push(text);
+      sqlArray.push(text);
+      sqlArray.push(text);
+      sqlArray.push(text);
+    }
+
+    // check if we are searching for a specific role (instead of any role)
+    if (role) {
+      sql += "AND role = ? ";
+      sqlArray.push(role);
+    }
+
+    // sort search results by username
+    sql += "ORDER BY userName ASC, " +
+      "userId ASC LIMIT ?;";
+
+    // get the number of results per page (plus the next cursor)
+    sqlArray.push(RESULTS_PER_PAGE + 1);
+
+    // perform the query
+    console.log(sql, sqlArray);
+    const results = await pool.query(sql, sqlArray);
+
+    // get the next cursor and return the correct number of users
+    if (results[0].length < RESULTS_PER_PAGE + 1) {
+
+      // if we have returned the last of the data then we return
+      // a null next cursor
+      users = results[0];
+      nextCursor.primary = "null";
+      nextCursor.secondary = "null";
+
+    } else {
+
+      // Our next cursor will store a primary and secondary value.
+      // The primary value is the main value we are sorting by.
+      // The secondary value is the user ID and it is used to sort when we
+      // have results with matching primary values.
+      users = results[0].slice(0, -1);
+      const nextPlan = results[0][RESULTS_PER_PAGE];
+
+      // set the primary and secondary strings
+      nextCursor.primary = String(nextPlan.userName);
+      nextCursor.secondary = String(nextPlan.userId);
+
+    }
+
+    return {
+      users: users,
+      nextCursor: nextCursor
+    };
+
+  } catch (err) {
+    console.error("Error searching Users");
+    throw Error(err);
+  }
+}
+exports.searchUsers = searchUsers;
+
