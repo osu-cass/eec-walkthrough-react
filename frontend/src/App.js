bb803d29de5daeeaa362ba637fb4823906c4f019
silverware13 <thomasza@oregonstate.edu>
2020-06-07 14:25:58 -0700

    Add basic edit user page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,77 +4,81 @@ import Home from "./pages/Home";
 import ManageUsers from "./pages/ManageUsers";
 import Search from "./pages/Search";
 import RegisterUser from "./pages/RegisterUser";
+import EditUser from "./pages/EditUser";
 import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
 import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
-	state = {
-		sidebarOpen: false,
-		loginStatusChange: false
-	};
+  state = {
+    sidebarOpen: false,
+    loginStatusChange: false
+  };
 
-	componentDidMount() {
-		//fetch all pages
-		//check if route exists within pages
-	}
+  componentDidMount() {
+    //fetch all pages
+    //check if route exists within pages
+  }
 
-	openSidebar = () => {
-		this.setState({ sidebarOpen: true });
-	};
+  openSidebar = () => {
+    this.setState({ sidebarOpen: true });
+  };
 
-	closeSidebar = () => {
-		this.setState({ sidebarOpen: false });
-	};
-	
-	handleLoginStatusChange = () => {
-		this.setState({ loginStatusChange: !this.state.loginStatusChange });
-	}
+  closeSidebar = () => {
+    this.setState({ sidebarOpen: false });
+  };
+  
+  handleLoginStatusChange = () => {
+    this.setState({ loginStatusChange: !this.state.loginStatusChange });
+  }
 
-	render() {
-		return (
-			<main>
-				<NavBar 
-					openSidebar={this.openSidebar}
-					handleLoginStatusChange={this.handleLoginStatusChange}
-				/>
-				<Sidebar
-					className={this.state.sidebarOpen ? "visible" : "hidden"}
-					loginStatusChange={this.state.loginStatusChange}
-					closeSidebar={this.closeSidebar}
-				/>
-				<Switch>
-					<Route
-						path='/subjects/:pageId'
-						render={(props) => (
-							<Subject {...props} pageId={props.match.params.pageId} />
-						)}
-					/>
-					<Route
-						path='/industries/:pageId'
-						render={(props) => (
-							<Subject {...props} pageId={props.match.params.pageId} />
-						)}
-					/>
-					<Route path='/search/:searchId'>
-						<Search />
-					</Route>
-					<Route path='/manage-users'>
-						<ManageUsers />
-					</Route>
-					<Route path='/register-user'>
-						<RegisterUser />
-					</Route>
-					<Route exact path='/'>
-						<Home />
-					</Route>
-					<Route path='*'>
-						<Home />
-					</Route>
-				</Switch>
-			</main>
-		);
-	}
+  render() {
+    return (
+      <main>
+        <NavBar 
+          openSidebar={this.openSidebar}
+          handleLoginStatusChange={this.handleLoginStatusChange}
+        />
+        <Sidebar
+          className={this.state.sidebarOpen ? "visible" : "hidden"}
+          loginStatusChange={this.state.loginStatusChange}
+          closeSidebar={this.closeSidebar}
+        />
+        <Switch>
+          <Route
+            path='/subjects/:pageId'
+            render={(props) => (
+              <Subject {...props} pageId={props.match.params.pageId} />
+            )}
+          />
+          <Route
+            path='/industries/:pageId'
+            render={(props) => (
+              <Subject {...props} pageId={props.match.params.pageId} />
+            )}
+          />
+          <Route path='/search/:searchId'>
+            <Search />
+          </Route>
+          <Route path='/manage-users'>
+            <ManageUsers />
+          </Route>
+          <Route path='/register-user'>
+            <RegisterUser />
+          </Route>
+          <Route path='/edit-user'>
+            <EditUser />
+          </Route>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='*'>
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
