ac571bd7c487a3abc5aeb0424624136b666dc2bc
silverware13 <thomasza@oregonstate.edu>
2020-06-15 20:06:42 -0700

    Modify the look of the main page header and card when the page is still awaiting approval

--- frontend/src/pages/ContentPage/PageDescription.js
+++ frontend/src/pages/ContentPage/PageDescription.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./PageDescription.css";
 
 // Card that describes the page and includes an image
 function PageDescription(props) {
   return (
-    <div className="my-3 p-3 bg-white card rounded shadow-sm">
+    <div className={`${props.approved ? "card-approved" : "card-review"} my-3 p-3 card rounded shadow-sm`}>
       <div className="row">
         <div className="col-8">
           <h5 className='font-weight-bold'>{props.header}</h5>
@@ -20,6 +21,7 @@ function PageDescription(props) {
 export default PageDescription;
 
 PageDescription.propTypes = {
+  approved: PropTypes.number,
   header: PropTypes.string,
   description: PropTypes.string,
   img: PropTypes.string
