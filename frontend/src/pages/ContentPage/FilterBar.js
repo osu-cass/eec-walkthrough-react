e1867fa12528593f57c4e74b610b94cea84c4b00
silverware13 <thomasza@oregonstate.edu>
2020-06-20 14:06:23 -0700

    Create filterbar function that gets the icon names using icon IDs

--- frontend/src/pages/ContentPage/FilterBar.js
+++ frontend/src/pages/ContentPage/FilterBar.js
@@ -1,77 +1,50 @@
-import React from "react";
-import Filter from "./Filter";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 
 // The bar inside of a header that is used for filtering out items
-class FilterBar extends React.Component {
-  state = {
-    iconsActivity: []
-  }
+function FilterBar(props) {
 
-  componentDidMount() {
-    const data = [];
-    let i;
-    for (i = 0; i < this.props.data.length; i++) {
-      data.push(true);
+  const [iconNames, setIconNames] = useState([]);
+
+  // get an array of the icon names that match the filter icon IDs
+  useEffect(() => {
+    const names = [];
+    for (let i = 0; i < props.filterIcons.length; i++) {
+      for (let j = 0; j < props.iconSet.length; j++) {
+        if (props.filterIcons[i] === props.iconSet[j].iconType) {
+          names.push(props.iconSet[j].typeName);
+        }
+      }
     }
-    this.setState({iconsActivity: data});
-  }
+    setIconNames(names);
+  }, [props.filterIcons, props.iconSet]);
 
-  /**
-	* Turn off/on a filter
-	* @param {Number} idx Index of filter item
-	*/
-  setActivity = (idx) => {
-    const data = [...this.state.iconsActivity];
-    data[idx] = !data[idx];
-    this.setState({iconsActivity: data});
-  }
+  return (
+    <div className="m-2 icons row">
+      {props.filterIcons.map((obj, i) => {
+        return (
+          <i
+            key={props.filterIcons[i]}
+            className={`fas fa-${iconNames[i]} ${
+              true ? "" : "fa-disabled"
+            } text-dark mr-3`}
+            onClick={() => {}}
+          />
+        );
+      })}
+      <i
+        id="reset"
+        className={`fas fa-undo text-dark mr-3`}
+        value="reset"
+        onClick={() => {}}
+      />
+    </div>
+  );
 
-  /**
-	* Reset all filters to show active
-	*/
-  resetFilter() {
-    let i;
-    const data = [...this.state.iconsActivity];
-    this.props.resetFilter(this.props.headerIndex);
-    for (i = 0; i < this.props.data.length; i++) {
-      data[i] = true;
-    }
-    this.setState({iconsActivity: data});
-  }
-
-  render() {
-    return (
-      <div className="m-2 icons row">
-        {this.props.data.map((obj, i) => { // for each tidbit type (pros/cons/etc.)
-          return (
-            <Filter
-              key={i}
-              idx={i}
-              id={obj.iconType}
-              headerIndex={this.props.headerIndex}
-              icon={obj.typeName}
-              setActivity={(i) => this.setActivity(i)}
-              active={this.state.iconsActivity[i]}
-              handleFilter={this.props.handleFilter}
-            />
-          );
-        })}
-        <i
-          id="reset"
-          className={`fas fa-undo text-dark mr-3`}
-          value="reset"
-          onClick={() => this.resetFilter()}
-        ></i>
-      </div>
-    );
-  }
 }
 export default FilterBar;
 
 FilterBar.propTypes = {
-  data: PropTypes.any,
-  resetFilter: PropTypes.any,
-  headerIndex: PropTypes.any,
-  handleFilter: PropTypes.any
+  iconSet: PropTypes.array,
+  filterIcons: PropTypes.array
 };
