bb6496129b19dfc0c7ac1411784bcb528d67678d
silverware13 <thomasza@oregonstate.edu>
2020-06-21 13:13:17 -0700

    Update front-end rendering and filtering to use Version Control data

--- frontend/src/pages/ContentPage/FilterBar.js
+++ frontend/src/pages/ContentPage/FilterBar.js
@@ -6,30 +6,62 @@ import "./FilterBar.css";
 function FilterBar(props) {
 
   const [iconNames, setIconNames] = useState([]);
+  const [tempIconNames, setTempIconNames] = useState([]);
 
   // get an array of the icon names that match the filter icon IDs
   useEffect(() => {
     const names = [];
+    const tempNames = [];
     for (let i = 0; i < props.filterIcons.length; i++) {
       for (let j = 0; j < props.iconSet.length; j++) {
         if (props.filterIcons[i] === props.iconSet[j].iconType) {
           names.push(props.iconSet[j].typeName);
+          break;
+        }
+      }
+    }
+    for (let i = 0; i < props.tempFilterIcons.length; i++) {
+      for (let j = 0; j < props.iconSet.length; j++) {
+        if (props.tempFilterIcons[i] === props.iconSet[j].iconType) {
+          tempNames.push(props.iconSet[j].typeName);
+          break;
         }
       }
     }
     setIconNames(names);
-  }, [props.filterIcons, props.iconSet]);
+    setTempIconNames(tempNames);
+  }, [props.filterIcons, props.tempFilterIcons, props.iconSet]);
 
-  return (
+  return props.mode ? (
+    <div className="m-2 icons row">
+      {props.tempFilterIcons.map((obj, i) => {
+        return (
+          <i
+            key={obj}
+            className={`fas fa-${tempIconNames[i]} ${
+              props.filterShow[obj] ? "" : "fa-disabled"
+            } text-dark mr-3`}
+            onClick={() => props.updateIcon(obj, props.filterShow[obj])}
+          />
+        );
+      })}
+      <i
+        id="reset"
+        className={`fas fa-undo text-dark mr-3`}
+        value="reset"
+        onClick={() => props.resetIcons()}
+      />
+    </div>
+  ) : (
     <div className="m-2 icons row">
       {props.filterIcons.map((obj, i) => {
         return (
           <i
-            key={props.filterIcons[i]}
+            key={obj}
             className={`fas fa-${iconNames[i]} ${
               props.filterShow[obj] ? "" : "fa-disabled"
             } text-dark mr-3`}
-            onClick={() => props.updateIcon(props.filterIcons[i], props.filterShow[obj])}
+            onClick={() => props.updateIcon(obj, props.filterShow[obj])}
           />
         );
       })}
@@ -50,5 +82,7 @@ FilterBar.propTypes = {
   resetIcons: PropTypes.func,
   filterShow: PropTypes.array,
   iconSet: PropTypes.array,
-  filterIcons: PropTypes.array
+  filterIcons: PropTypes.array,
+  tempFilterIcons: PropTypes.array,
+  mode: PropTypes.number
 };
