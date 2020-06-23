6d374bd3b52632a55520766576859382214e22fb
silverware13 <thomasza@oregonstate.edu>
2020-06-22 21:57:00 -0700

    Add page publishing for version controlled pages

--- backend/routes/pages.js
+++ backend/routes/pages.js
@@ -25,7 +25,8 @@ const {
   deletePage,
   updatePage,
   addSubject,
-  deleteSubject
+  deleteSubject,
+  publishPage
 } = require("../models/pages");
 
 
@@ -406,6 +407,53 @@ app.delete("/industries/:industryId/subjects/:subjectId", requireAuth, industryS
         res.status(404).send({error: "Industry page not found."});
       } else if (results.error === 3) {
         res.status(404).send({error: "This subject is not part of this industry."});
+      } else {
+        res.status(500).send({error: "An internal server error occurred. Please try again later."});
+      }
+
+    }
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({error: "An internal server error occurred. Please try again later."});
+  }
+
+});
+
+
+// publish a page
+app.post("/:pageId/publish", getUserID, getPageVal.validation, async (req, res) => {
+
+  try {
+
+    console.log("Publish a page");
+
+    const pageId = req.params.pageId;
+
+    // confirm that the request is valid
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.error(errors.array());
+      return res.status(422).json({errors: errors.array()});
+    }
+
+    // make sure the user is allowed to perform this action
+    if (!await roleCheck(4, req.auth.userId)) {
+      res.status(401).send({error: "Unauthorized user attempting to publish a page."});
+      return;
+    }
+
+    // publish a page
+    const results = await publishPage(pageId);
+
+    if (results.pageId >= 0) {
+      res.status(200).send(results);
+    } else {
+
+      if (results.error === 1) {
+        res.status(404).send({error: "Page not found."});
+      } else if (results.error === 2) {
+        res.status(403).send({error: "A page with the same name and type already exists."});
       } else {
         res.status(500).send({error: "An internal server error occurred. Please try again later."});
       }
