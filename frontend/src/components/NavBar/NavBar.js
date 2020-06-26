b5f4b5939ecf0141c844cc0051c217765cf431c4
silverware13 <thomasza@oregonstate.edu>
2020-06-25 17:58:58 -0700

    Restyle navigation bar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import PageSearch from "./PageSearch";
 import PropTypes from "prop-types";
 import UserIcon from "./UserIcon";
 import {getProfile} from "../../utilities/cookieAuth";
+import "./NavBar.css";
 
 // navigation bar that appears at the top of the page
 function NavBar (props) {
@@ -28,20 +29,32 @@ function NavBar (props) {
     <div className="navigation-bar">
 
       <nav className="navbar navbar-dark bg-dark align-items-center">
-        <h3 className="text-white nav-item align-items-center mt-2">
+        <div className="row text-white nav-item align-items-center">
           <button
-            className="text-info mr-2 mx-2 border-0 bg-dark"
+            className="nav-hamburger text-info ml-4 border-0 bg-dark"
             href="#"
             onClick={props.openSidebar}
           >
-            <i className="fas fa-bars"></i>
+            <i className="fas fa-bars fa-3x" />
           </button>
-          <a className="mx-2 align-middle" href="/">
-            EEC Walkthrough (Demo)
-          </a>
-        </h3>
+          <img
+            className="osu-logo ml-4 mr-5"
+            src={"/images/OSU.png"}
+            alt={"Oregon State University"}
+            title={"Oregon State University"}
+          />
+          <div className="column">
+            <h4 className="nav-header-top">
+              Energy Efficiency Center
+            </h4>
+            <h4 className="nav-header-bottom">
+              Industrial Walkthrough Checklist &amp; Reference (Demo)
+            </h4>
+          </div>
+            
+        </div>
 
-        <div className="d-flex nav-item align-items-center">
+        <div className="d-flex nav-item align-items-center mt-3">
           <PageSearch />
           <UserIcon onLogin={() => updateUser()} username={username} role={role} />
           <Login onLogin={() => updateUser()} role={role} />
