0b36ba18576471a6d7fcd7cf2e433d155933cbc5
silverware13 <thomasza@oregonstate.edu>
2020-07-02 15:45:28 -0700

    Update front end to allow sorting and ordering of user searches

--- frontend/src/pages/ManageUsers/UserSearchResults.js
+++ frontend/src/pages/ManageUsers/UserSearchResults.js
@@ -9,39 +9,96 @@ import "./UserSearchResults.css";
 // search results for a user search
 function UserSearchResults(props) {
 
+  // updates the sorting order of the search results
+  function changeSort(sortValue, alternateOrder) {
+    if (alternateOrder) {
+      props.onChangeSort(sortValue, 1 - props.searchFields.orderValue);
+    } else {
+      props.onChangeSort(sortValue, 1);
+    }
+  }
+
   if (props.users.length) {
     return (
       <div className="table-container">
         <table className="user-table shadow">
           <thead>
             <tr>
-              <th className="user-data" style={{width: "18%"}}>username</th>
-              <th className="user-data" style={{width: "18%"}}>Name</th>
-              <th className="user-data" style={{width: "10%"}}>User ID</th>
-              <th className="user-data" style={{width: "18%"}}>Email</th>
-              <th className="user-data" style={{width: "18%"}}>Created</th>
-              <th className="user-data" style={{width: "18%"}}>Role</th>
+              {props.searchFields.sortValue === 0 ? (
+                <th className="student-plans-data active-sort" onClick={() => changeSort(0, true)} style={{width: "18%"}}>
+                  Username <small>{props.searchFields.orderValue ? "▲" : "▼" }</small>
+                </th>
+              ) : (
+                <th className="student-plans-data" onClick={() => changeSort(0, false)} style={{width: "18%"}}>
+                  Username <small>▼</small>
+                </th>
+              )}
+              {props.searchFields.sortValue === 1 ? (
+                <th className="student-plans-data active-sort" onClick={() => changeSort(1, true)} style={{width: "18%"}}>
+                  Name <small>{props.searchFields.orderValue ? "▲" : "▼" }</small>
+                </th>
+              ) : (
+                <th className="student-plans-data" onClick={() => changeSort(1, false)} style={{width: "18%"}}>
+                  Name <small>▼</small>
+                </th>
+              )}
+              {props.searchFields.sortValue === 2 ? (
+                <th className="student-plans-data active-sort" onClick={() => changeSort(2, true)} style={{width: "10%"}}>
+                  User ID <small>{props.searchFields.orderValue ? "▲" : "▼" }</small>
+                </th>
+              ) : (
+                <th className="student-plans-data" onClick={() => changeSort(2, false)} style={{width: "10%"}}>
+                  User ID <small>▼</small>
+                </th>
+              )}
+              {props.searchFields.sortValue === 3 ? (
+                <th className="student-plans-data active-sort" onClick={() => changeSort(3, true)} style={{width: "18%"}}>
+                  Email <small>{props.searchFields.orderValue ? "▲" : "▼" }</small>
+                </th>
+              ) : (
+                <th className="student-plans-data" onClick={() => changeSort(3, false)} style={{width: "18%"}}>
+                  Email <small>▼</small>
+                </th>
+              )}
+              {props.searchFields.sortValue === 4 ? (
+                <th className="student-plans-data active-sort" onClick={() => changeSort(4, true)} style={{width: "18%"}}>
+                  Created <small>{props.searchFields.orderValue ? "▲" : "▼" }</small>
+                </th>
+              ) : (
+                <th className="student-plans-data" onClick={() => changeSort(4, false)} style={{width: "18%"}}>
+                  Created <small>▼</small>
+                </th>
+              )}
+              {props.searchFields.sortValue === 5 ? (
+                <th className="student-plans-data active-sort" onClick={() => changeSort(5, true)} style={{width: "18%"}}>
+                  Role <small>{props.searchFields.orderValue ? "▲" : "▼" }</small>
+                </th>
+              ) : (
+                <th className="student-plans-data" onClick={() => changeSort(5, false)} style={{width: "18%"}}>
+                  Role <small>▼</small>
+                </th>
+              )}
             </tr>
           </thead>
           <tbody>
             {props.users.map((user, index) =>
-              <tr key={user.userId}>
-                <td className="user-data" key={user.userId + "a"}>
+              <tr key={user.userId + "a"}>
+                <td className="user-data" key={user.userId + "b"}>
                   {user.username}
                 </td>
-                <td className="user-data" key={user.userId + "b"}>
+                <td className="user-data" key={user.userId + "c"}>
                   {user.firstName + " " + user.lastName}
                 </td>
-                <td className="user-data" key={user.userId + "c"}>
+                <td className="user-data" key={user.userId + "d"}>
                   {user.userId}
                 </td>
-                <td className="user-data" key={user.userId + "d"}>
+                <td className="user-data" key={user.userId + "e"}>
                   {user.email}
                 </td>
-                <td className="user-data" key={user.userId + "e"}>
+                <td className="user-data" key={user.userId + "f"}>
                   {formatTime(user.created)}
                 </td>
-                <td className="user-data" key={user.userId + "f"}>
+                <td className="user-data" key={user.userId + "g"}>
                   <UserSelectRole role={user.role} userId={user.userId} index={index}
                     username={user.firstName + " " + user.lastName} onLoading={load => props.onLoading(load)}
                   />
@@ -81,6 +138,8 @@ UserSearchResults.propTypes = {
   history: PropTypes.object,
   users: PropTypes.array,
   cursor: PropTypes.object,
+  searchFields: PropTypes.object,
   onLoadMore: PropTypes.func,
+  onChangeSort: PropTypes.func,
   onLoading: PropTypes.func
 };
