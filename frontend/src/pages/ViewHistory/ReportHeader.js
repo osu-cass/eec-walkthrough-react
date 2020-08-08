245929e0b3c5f5f7c369ff3410efa2e84e52c906
silverware13 <thomasza@oregonstate.edu>
2020-08-07 22:14:46 -0700

    Add postitive diff tracking for headers

--- frontend/src/pages/ViewHistory/ReportHeader.js
+++ frontend/src/pages/ViewHistory/ReportHeader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {formatTime} from "../../utilities/formatTime";
+import HighlightText from "../ContentPage/HighlightText";
 import "./ReportHeader.css";
 
 // Header history for a single header
@@ -8,12 +9,22 @@ function ReportHeader(props) {
 
   return (
     <div className='text-left mx-2'>
-      <div className="version-container p-2 m-3 border border-dark rounded">
+      <div className={`version-container p-2 m-3 border border-dark rounded ${props.header.oldVersion ? "" : "highlight-report-header"}`}>
         <h4 className="report-header-special-text pl-4 pt-4">Header</h4>
         <h5 className="report-header-special-text pl-4">{props.header.categoryName} &rarr; {props.header.pageName} &rarr; {props.header.title}</h5>
         <span className="report-header-special-text pl-4">Updated {formatTime(props.header.created)}</span>
         <div className="m-4">
-          <h3 className="font-weight-bold">{props.header.title}</h3>
+          {props.header.oldVersion ? (
+            <HighlightText
+              newMode={true}
+              newText={props.header.title}
+              oldText={props.header.oldVersion.title}
+              elementType={1}
+              newId={props.newId}
+            />
+          ) : (
+            <h3 className="font-weight-bold">{props.header.title}</h3>
+          )}
         </div>
       </div>
     </div>
