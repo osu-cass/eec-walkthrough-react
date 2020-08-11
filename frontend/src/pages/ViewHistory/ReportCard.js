43de077f46027ba232a88bca5e945058f8a3ce0e
silverware13 <thomasza@oregonstate.edu>
2020-08-10 22:11:09 -0700

    Add card spliting to reports

--- frontend/src/pages/ViewHistory/ReportCard.js
+++ frontend/src/pages/ViewHistory/ReportCard.js
@@ -34,7 +34,89 @@ function ReportCard(props) {
     // eslint-disable-next-line
   }, [props.card]);
 
-  return (
+  return props.removeMode ? (
+    <div className="text-left mx-2 row">
+
+      {props.card.oldVersion ? (
+        <div className="col">
+          <div className={`version-container p-2 m-3 border border-dark rounded text-wrap`}>
+            <h4 className="report-card-special-text pl-3 pt-4">Card</h4>
+            <h5 className="report-card-special-text pl-3">{props.card.oldVersion.categoryName} &rarr; {props.card.oldVersion.pageName} &rarr; {props.card.oldVersion.headerName} &rarr; {props.card.oldVersion.title}</h5>
+            <span className="report-card-special-text pl-3">Updated {formatTime(props.card.oldVersion.created)}</span>
+            <div className="m-3">
+              <HighlightText
+                newMode={false}
+                newText={props.card.title}
+                oldText={props.card.oldVersion.title}
+                elementType={1}
+                newId={props.newId}
+              />
+              {props.card.cardType === 1 || props.card.cardType === 11 ? (
+                <ThumbnailGallery items={oldImageItems} compareMode={2} otherItems={imageItems} />
+              ) : (
+                <BasicItems items={props.card.oldVersion.items} mode={0} reviewing={true} compareMode={2} otherItems={props.card.items} setCheck={() => {}} />
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="col">
+          <div className={`version-container p-2 m-3 border border-dark rounded text-wrap hidden-report`}>
+            <h4 className="report-card-special-text pl-3 pt-4">Card</h4>
+            <h5 className="report-card-special-text pl-3">{props.card.categoryName} &rarr; {props.card.pageName} &rarr; {props.card.headerName} &rarr; {props.card.title}</h5>
+            <span className="report-card-special-text pl-3">Updated {formatTime(props.card.created)}</span>
+            <div className="m-3">
+              <span className="report-card-span highlight-new-content">{props.card.title}</span>
+              {props.card.cardType === 1 || props.card.cardType === 11 ? (
+                <ThumbnailGallery items={imageItems} compareMode={3} />
+              ) : (
+                <BasicItems items={props.card.items} mode={0} reviewing={true} compareMode={3} setCheck={() => {}} />
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="col">
+        <div className={`version-container p-2 m-3 border border-dark rounded text-wrap`}>
+          <h4 className="report-card-special-text pl-3 pt-4">Card</h4>
+          <h5 className="report-card-special-text pl-3">{props.card.categoryName} &rarr; {props.card.pageName} &rarr; {props.card.headerName} &rarr; {props.card.title}</h5>
+          <span className="report-card-special-text pl-3">Updated {formatTime(props.card.created)}</span>
+          <div className="m-3">
+            {props.card.oldVersion ? (
+              <HighlightText
+                newMode={true}
+                newText={props.card.title}
+                oldText={props.card.oldVersion.title}
+                elementType={1}
+                newId={props.newId}
+              />
+            ) : (
+              <span className="report-card-span highlight-new-content">{props.card.title}</span>
+            )}
+            {props.card.cardType === 1 || props.card.cardType === 11 ? (
+              <Fragment>
+                {props.card.oldVersion ? (
+                  <ThumbnailGallery items={imageItems} compareMode={1} otherItems={oldImageItems} />
+                ) : (
+                  <ThumbnailGallery items={imageItems} compareMode={3} />
+                )}
+              </Fragment>
+            ) : (
+              <Fragment>
+                {props.card.oldVersion ? (
+                  <BasicItems items={props.card.items} mode={0} reviewing={true} compareMode={1} otherItems={props.card.oldVersion.items} setCheck={() => {}} />
+                ) : (
+                  <BasicItems items={props.card.items} mode={0} reviewing={true} compareMode={3} setCheck={() => {}} />
+                )}
+              </Fragment>
+            )}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  ) : (
     <div className="text-left mx-2">
       <div className={`version-container p-2 m-3 border border-dark rounded text-wrap`}>
         <h4 className="report-card-special-text pl-3 pt-4">Card</h4>
@@ -79,5 +161,6 @@ export default ReportCard;
 
 ReportCard.propTypes = {
   card: PropTypes.object,
-  newId: PropTypes.number
+  newId: PropTypes.number,
+  removeMode: PropTypes.bool
 };
