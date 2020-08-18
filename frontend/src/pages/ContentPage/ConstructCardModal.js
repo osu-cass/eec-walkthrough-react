9139751d702c440aa86edf5b0a32581606c8e702
silverware13 <thomasza@oregonstate.edu>
2020-08-18 14:59:21 -0700

    Make item edit / delete bar sticky

--- frontend/src/pages/ContentPage/ConstructCardModal.js
+++ frontend/src/pages/ContentPage/ConstructCardModal.js
@@ -1046,55 +1046,57 @@ function ConstructCardModal(props) {
 
           <div className="font-weight-bold mb-2">Items</div>
 
-          <div className="mr-3 mb-3">
-            <button className="btn btn-danger btn ml-2"
-              onClick={() => deleteItem()}
-            >
-              <i className="fas fa-fw fa-times mr-2" />
-              Delete Item
-            </button>
+          <div className="item-button-bar card sticky-top py-2 px-2 mb-3">
+            <div>
+              <button className="btn btn-danger btn ml-2"
+                onClick={() => deleteItem()}
+              >
+                <i className="fas fa-fw fa-times mr-2" />
+                Delete Item
+              </button>
 
-            <button className="btn btn-info internal-item-button btn ml-2"
-              onClick={() => toggleInternal()}
-            >
-              <i className="fas fa-fw fa-unlock mr-2" />
-              Toggle Internal
-            </button>
+              <button className="btn btn-info internal-item-button btn ml-2"
+                onClick={() => toggleInternal()}
+              >
+                <i className="fas fa-fw fa-unlock mr-2" />
+                Toggle Internal
+              </button>
 
-            <button className="btn btn-info copy-paste-button btn ml-2"
-              onClick={() => copyItem()}
-            >
-              <i className="fas fa-fw fa-copy mr-2" />
-              Copy Item
-            </button>
+              <button className="btn btn-info copy-paste-button btn ml-2"
+                onClick={() => copyItem()}
+              >
+                <i className="fas fa-fw fa-copy mr-2" />
+                Copy Item
+              </button>
 
-            <button className="btn btn-primary btn ml-2"
-              onClick={() => changeIndent(-1)}
-            >
-              <i className="fas fa-fw fa-minus mr-2" />
-              Unindent
-            </button>
+              <button className="btn btn-primary btn ml-2"
+                onClick={() => changeIndent(-1)}
+              >
+                <i className="fas fa-fw fa-minus mr-2" />
+                Unindent
+              </button>
 
-            <button className="btn btn-primary btn ml-2"
-              onClick={() => changeIndent(1)}
-            >
-              <i className="fas fa-fw fa-plus mr-2" />
-              Indent
-            </button>
+              <button className="btn btn-primary btn ml-2"
+                onClick={() => changeIndent(1)}
+              >
+                <i className="fas fa-fw fa-plus mr-2" />
+                Indent
+              </button>
 
-            <button className="btn btn-success btn ml-2"
-              onClick={() => changeOrder(true)}
-            >
-              <i className="fas fa-fw fa-arrow-up mr-2" />
-              Move Up
-            </button>
+              <button className="btn btn-success btn ml-2"
+                onClick={() => changeOrder(true)}
+              >
+                <i className="fas fa-fw fa-arrow-up mr-2" />
+                Move Up
+              </button>
 
-            <button className="btn btn-success btn ml-2"
-              onClick={() => changeOrder(false)}
-            >
-              <i className="fas fa-fw fa-arrow-down mr-2" />
-              Move Down
-            </button>
+              <button className="btn btn-success btn ml-2"
+                onClick={() => changeOrder(false)}
+              >
+                <i className="fas fa-fw fa-arrow-down mr-2" />
+                Move Down
+              </button>
+            </div>
           </div>
 
           {/* Item Input Fields */}
