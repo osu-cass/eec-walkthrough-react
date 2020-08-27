0ab2a4e1d9d60de8ebb2dd506ed3c8b3b411deb9
silverware13 <thomasza@oregonstate.edu>
2020-08-27 01:38:52 -0700

    Fix issue where 'move buttons' would sometimes not have rounded corners

--- frontend/src/pages/ContentPage/OrderObjectButton.js
+++ frontend/src/pages/ContentPage/OrderObjectButton.js
@@ -1,5 +1,6 @@
 import React, {Fragment} from "react";
 import PropTypes from "prop-types";
+import "./OrderObjectButton.css";
 
 // A button used to rearrange cards and headers
 function OrderObjectButton(props) {
@@ -7,13 +8,13 @@ function OrderObjectButton(props) {
   return (props.edited && !props.publishedMode) || (props.approved && props.publishedMode) ? (
     <Fragment>
       {props.up ? (
-        <button className={`btn btn-success btn-sm ml-2 mr-1 px-3`}
+        <button className={`btn btn-success btn-order-objects btn-sm ml-2 mr-1 px-3`}
           onClick={() => props.handleMove(props.objectId, props.up, props.publishedMode)}
         >
           <i className='fas fa-fw fa-arrow-up' />
         </button>
       ) : (
-        <button className={`btn btn-success btn-sm ml-1 mr-2 px-3`}
+        <button className={`btn btn-success btn-order-objects btn-sm ml-1 mr-2 px-3`}
           onClick={() => props.handleMove(props.objectId, props.up, props.publishedMode)}
         >
           <i className='fas fa-fw fa-arrow-down' />
