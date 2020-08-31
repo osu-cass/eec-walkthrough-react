ea36e751ee502b36012b86919c49452703a2fc87
silverware13 <thomasza@oregonstate.edu>
2020-08-31 00:34:54 -0700

    Make it so editors can only submit sources

--- frontend/src/pages/ContentPage/AddSource.js
+++ frontend/src/pages/ContentPage/AddSource.js
@@ -5,7 +5,7 @@ import Error from "../../components/General/Error";
 import LoadingOverlay from "../../components/General/LoadingOverlay";
 import {logout} from "../../utilities/cookieAuth";
 
-// Button and modal that allows a user to add a source to a page
+// Button and modal that allows users to manage sources for a given page
 function AddSource(props) {
 
   const [sources, setSources] = useState([]);
@@ -114,22 +114,21 @@ function AddSource(props) {
     setSources(editedSources);
   }
 
-
-  // Submit new source info
-  async function handleSubmit(e) {
+  // Submit a single new source
+  async function handleSubmitSingle(e) {
     e.preventDefault();
-    const editedSources = [...sources];
 
-    // See if the sources meet the minimum required text
-    for (let i = 0; i < editedSources.length; i++) {
-      if (!editedSources[i].text.length) {
-        setErrorMessage("Error: Text describing the source is blank for source #" + (i + 1));
-        return;
-      }
+    const text = document.getElementById("single-source-text").value;
+    const url = document.getElementById("single-source-url").value;
+
+    if (!text.length) {
+      setErrorMessage("Error: Text describing the source is blank");
+      return;
     }
 
     const data = {
-      sources: sources
+      text: text,
+      url: url
     };
 
     const results = await fetch(`/api/sources/page/${props.pageId}`, {
@@ -160,8 +159,54 @@ function AddSource(props) {
     setLoading(false);
   }
 
-  return props.role >= 3 && props.mode === 1 ? (
-    <div className="text-center mx-2 my-auto">
+  // Submit all page source info
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const editedSources = [...sources];
+
+    // See if the sources meet the minimum required text
+    for (let i = 0; i < editedSources.length; i++) {
+      if (!editedSources[i].text.length) {
+        setErrorMessage("Error: Text describing the source is blank for source #" + (i + 1));
+        return;
+      }
+    }
+
+    const data = {
+      sources: sources
+    };
+
+    const results = await fetch(`/api/sources/all/page/${props.pageId}`, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    });
+
+    if (results.ok) {
+
+      // refresh the page
+      window.location.reload();
+
+    } else {
+
+      const obj = await results.json();
+
+      if (results.status === 401) {
+        logout();
+        window.location.href = "/";
+      } else if (results.status === 500 || typeof obj.error === "undefined") {
+        setErrorMessage("An internal server error occurred. Please try again later.");
+      } else {
+        setErrorMessage(obj.error);
+      }
+
+    }
+    setLoading(false);
+  }
+
+  if (props.role === 4) {
+
+    return <div className="text-center mx-2 my-auto">
       <LoadingOverlay loading={loading} />
       <Button size="sm" variant="info" onClick={() => handleShowModal()}>
         <i
@@ -239,9 +284,78 @@ function AddSource(props) {
         </Modal.Footer>
       </Modal>
     </div>
-  ) : (
-    null
-  );
+
+  } else if (props.role === 3) {
+
+    return <div className="text-center mx-2 my-auto">
+      <LoadingOverlay loading={loading} />
+      <Button size="sm" variant="info" onClick={() => handleShowModal()}>
+        <i
+          className="fas fa-book text-white ml-auto mr-2"
+          style={{transform: "scale(1.5)"}}
+        />
+        <span className="text-white">Add Source</span>
+      </Button>
+      <Modal show={showModal} onHide={() => handleCloseModal()} dialogClassName="modal-width">
+        <Modal.Header>
+          <h5 className="modal-title font-weight-bold" id="exampleModalLabel">Add Source</h5>
+          <Button variant="none" onClick={() => handleCloseModal()}>
+            <span aria-hidden="true">&times;</span>
+          </Button>
+        </Modal.Header>
+
+        <Modal.Body>
+
+          <Row className="mb-2">
+            <div className="input-group">
+              <FormControl
+                id="single-source-text"
+                className="ml-3"
+                as="textarea"
+                rows="3"
+                maxLength="5000"
+                placeholder="Citation Text"
+                aria-label="Citation Text"
+                aria-describedby="basic-addon1"
+                required
+              />
+              <FormControl
+                id="single-source-url"
+                className="mr-3"
+                as="textarea"
+                rows="3"
+                maxLength="5000"
+                placeholder="URL (optional)"
+                aria-label="URL"
+                aria-describedby="basic-addon1"
+                required
+              />
+            </div>
+          </Row>
+
+          <Row>
+            <div className='col-3' />
+            <div className='col-6 mt-2'>
+              <Error
+                message={errorMessage}
+              />
+            </div>
+          </Row>
+
+        </Modal.Body>
+
+        <Modal.Footer className="modal-footer">
+          <Button variant="primary" onClick={(e) => handleSubmitSingle(e)}>Submit Source</Button>
+          <Button variant="secondary" onClick={() => handleCloseModal()}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+
+  } else {
+
+    return null
+
+  }
 
 }
 export default AddSource;
