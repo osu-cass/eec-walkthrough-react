ad4e531c94260b00959b9a613c6fed0bf3db0a02
silverware13 <thomasza@oregonstate.edu>
2020-09-03 00:27:40 -0700

    Add api endpoint for handling file uploads

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -2,7 +2,6 @@
 // Description: handles routing files
 
 const multer = require("multer");
-const upload = multer({dest: "uploads/"});
 const express = require("express");
 const crypto = require("crypto");
 const app = express();
@@ -10,24 +9,20 @@ const {
   requireAuth,
   roleCheck
 } = require("../services/authentication/cookieAuth");
-const {
-  uploadFile
-} = require("../models/files");
 
-app.post('/single', upload.single("image"), (req, res) => {
-  console.log(req.file)
-  try {
-    res.send(req.file);
-  }catch(err) {
-    res.send(400);
-  }
-});
 
-// file handling middleware
-/*
+// valid image types
+const imageTypes = {
+  "image/jpeg": "jpg",
+  "image/png" : "png",
+  "image/gif" : "gif"
+};
+
+
+// define file upload settings
 const upload = multer({
   storage: multer.diskStorage({
-    destination: "$(__dirname}/uploads",
+    destination: "../frontend/public/uploads/",
     filename: (req, file, callback) => {
       // store image files with the correct extension.
       const basename = crypto.pseudoRandomBytes(16).toString("hex");
@@ -40,88 +35,37 @@ const upload = multer({
     callback(null, !!imageTypes[file.mimetype]);
   }
 });
-*/
-
-// upload a new file
-// app.post("/upload", requireAuth, async (req, res) => {
 
 
-
-
-
-/*
-app.post("/upload", async (req, res) => {
-  console.log("UPLOAD")
-  const imageTypes = {
-    "image/jpeg": "jpg",
-    "image/png": "png"
-  };
-
-  var storage = multer.diskStorage({
-    destination: `${__dirname}/../uploads`,
-    filename: (req, file, callback) => {
-      // store image files with the correct extension.
-      const basename = crypto.pseudoRandomBytes(16).toString("hex");
-      const extension = imageTypes[file.mimetype];
-      callback(null, `${basename}.${extension}`);
-    }
-  });
-  var upload = multer({ storage : storage}).any();
-
-  console.log("getFileName", storage.getFileName)
-
-  upload(req,res,function(err) {
-      if(err) {
-        console.log(err);
-        console.log("Error uploading file.")
-        res.status(500).send({error: "An internal server error occurred. Please try again later."});
-      } else {
-        console.log("File has been uploaded.")
-        res.status(200).send({url: "File uploaded"});
-      }
-  });
-*/
-
-
-
-
-  /*
-  console.log(req.body)
-  if (!req.file) {
-    console.log("No file received")
-    res.status(500).send({error: "An internal server error occurred. Please try again later."})
+// confirm that the user is a editor or admin
+const checkUser = async (req, res, next) => {
+  if (!await roleCheck(3, req.auth.userId)) {
+    res.status(401).send({error: "Unauthorized user attempting to upload a file."});
   } else {
-    res.status(200).send({url: "this url"});
-  }*/
+    next();
+  }
+};
 
-  /*
+
+// upload a single file
+app.post('/single', requireAuth, checkUser, upload.single("image"), async (req, res) => {
+
   try {
 
-    const files = req.files;
-    console.log(files)
-    console.log("Upload a new file");
+    console.log("Upload a file");
 
-    // make sure the user is allowed to perform this action
-    if (!await roleCheck(3, req.auth.userId)) {
-      res.status(401).send({error: "Unauthorized user attempting to upload a file."});
-      return;
-    }
-
-    // upload a new file
-    const results = await uploadFile(files);
-
-    if (results.url) {
-      res.status(201).send(results);
+    if (req.file) {
+      res.status(201).json({url: `/uploads/${req.file.filename}`});
     } else {
-      res.status(500).send({error: "An internal server error occurred. Please try again later."});
+      res.status(401).send({error: "Invalid file"});
     }
 
-  } catch (err) {
+  } catch(err) {
     console.error(err);
     res.status(500).send({error: "An internal server error occurred. Please try again later."});
   }
-  });
-*/
+
+});
 
 
 module.exports = app;
