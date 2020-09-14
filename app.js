9c36239d4ab9601edbf88ee78ee81b94f4762b06
silverware13 <thomasza@oregonstate.edu>
2020-09-14 02:52:53 -0700

    Allow https handling for API requests

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ require("dotenv").config({silent: process.env.NODE_ENV === "production"});
 
 const {pool} = require("./services/database/mysqlPool");
 const app = require("./routes/index");
+const https = require("https");
+const fs = require("fs");
 
 console.log("Server JavaScript start");
 console.log("Running in", process.env.NODE_ENV, "mode");
+
+const port = process.env.PORT || 2222;
+
+const options = {
+  key: fs.readFileSync("walkthrough.key"),
+  cert: fs.readFileSync("walkthrough.cer")
+};
 
 // confirm that connection was made to the database
 async function testConnection(pool, attempt, callback) {
@@ -27,9 +36,8 @@ async function testConnection(pool, attempt, callback) {
 }
 
 // listen for incoming requests
-const port = process.env.PORT || 2222;
 testConnection(pool, 1, () => {
-  app.listen(port, () => {
+  http.createServer(options, app).listen(port, () => {
     console.log("Server is listening on port", port, "\n");
   });
 });
--- client/src/utilities/constants.js
+++ client/src/utilities/constants.js
@@ -2,3 +2,4 @@
 // Description: constants that need to be easily referenced
 
 export const APIURL = "https://walkthrough.eec.oregonstate.edu:1111/api";
+// export const APIURL = "http://localhost:1111/api";
