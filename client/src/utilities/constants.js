aabd55ee6c404842085f91dfd4964db94935b6af
silverware13 <thomasza@oregonstate.edu>
2020-09-14 05:48:03 -0700

    Update port for API

--- client/src/utilities/constants.js
+++ client/src/utilities/constants.js
@@ -1,5 +1,5 @@
 // File: constants.js
 // Description: constants that need to be easily referenced
 
-export const APIURL = "https://walkthrough.eec.oregonstate.edu/api";
+export const APIURL = "https://walkthrough.eec.oregonstate.edu:1111/api";
 // export const APIURL = "http://localhost:1111/api";
