eaf98d503c09711dd32a5662cd888f24628ab794
silverware13 <thomasza@oregonstate.edu>
2020-09-14 01:22:52 -0700

    Work on replacing proxy

--- client/src/utilities/constants.js
+++ client/src/utilities/constants.js
@@ -1,4 +1,4 @@
 // File: constants.js
 // Description: constants that need to be easily referenced
 
-export const APIURL = "https://walkthrough.eec.oregonstate.edu/api";
+export const APIURL = "http://walkthrough.eec.oregonstate.edu:1111/api";
