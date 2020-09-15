654ceb02d3cda8299187fcee156d67b96af58783
silverware13 <thomasza@oregonstate.edu>
2020-09-14 18:44:37 -0700

    Ensure developer mode is still stable

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require("dotenv").config({silent: process.env.NODE_ENV === "production"});
 const {pool} = require("./services/database/mysqlPool");
 const app = require("./routes/index");
 const https = require("https");
+const http = require("http");
 const fs = require("fs");
 
 console.log("Server JavaScript start");
@@ -14,10 +15,14 @@ console.log("Running in", process.env.NODE_ENV, "mode");
 
 const port = process.env.PORT || 2222;
 
-const options = {
-  key: fs.readFileSync("walkthrough.key"),
-  cert: fs.readFileSync("walkthrough.cer")
-};
+let options = {};
+
+if (process.env.NODE_ENV === "production") {
+  options = {
+    key: fs.readFileSync("walkthrough.key"),
+    cert: fs.readFileSync("walkthrough.cer")
+  };
+}
 
 // confirm that connection was made to the database
 async function testConnection(pool, attempt, callback) {
@@ -37,9 +42,17 @@ async function testConnection(pool, attempt, callback) {
 
 // listen for incoming requests
 testConnection(pool, 1, () => {
-  https.createServer(options, app).listen(port, () => {
-    console.log("Server is listening on port", port, "\n");
-  });
+
+  // for production we use https, for development we use http
+  if (process.env.NODE_ENV === "production") {
+    https.createServer(options, app).listen(port, () => {
+      console.log("Server is listening on port", port, "\n");
+    });
+  } else {
+    http.createServer(app).listen(port, () => {
+      console.log("Server is listening on port", port, "\n");
+    });
+  }
 });
 
 module.exports = app;
--- client/src/utilities/constants.js
+++ client/src/utilities/constants.js
@@ -1,5 +1,10 @@
 // File: constants.js
 // Description: constants that need to be easily referenced
 
-export const APIURL = "https://walkthrough.eec.oregonstate.edu:1111/api";
-// export const APIURL = "http://localhost:1111/api";
+export let APIURL;
+
+if (process.env.NODE_ENV === "development") {
+  APIURL = "/api";
+} else {
+  APIURL = "https://walkthrough.eec.oregonstate.edu:1111/api";
+}
