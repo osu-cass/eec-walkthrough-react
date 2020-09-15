0d908cd1b4dfdae3ee8054a9244bc7865052824c
silverware13 <thomasza@oregonstate.edu>
2020-09-14 18:44:37 -0700

    Ensure developer mode is still stable

--- services/authentication/cookieAuth.js
+++ services/authentication/cookieAuth.js
@@ -138,7 +138,36 @@ exports.generateAuthToken = generateAuthToken;
 // Set user cookies for general data and authentication
 function setAuthCookie(res, token, username, userId, role) {
 
-  /* if (process.env.NODE_ENV === "production") { */
+  if (process.env.NODE_ENV === "development") {
+
+    res.setHeader("Set-Cookie", [
+      cookie.serialize("username_ck", username, {
+        path: "/",
+        sameSite: true,
+        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
+        maxAge: COOKIE_EXPIRES_MS / 1000,
+      }),
+      cookie.serialize("userId_ck", userId, {
+        path: "/",
+        sameSite: true,
+        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
+        maxAge: COOKIE_EXPIRES_MS / 1000,
+      }),
+      cookie.serialize("role_ck", role, {
+        path: "/",
+        sameSite: true,
+        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
+        maxAge: COOKIE_EXPIRES_MS / 1000,
+      }),
+      cookie.serialize("auth_ck", token, {
+        path: "/",
+        sameSite: true,
+        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
+        maxAge: COOKIE_EXPIRES_MS / 1000,
+      })
+    ]);
+
+  } else {
 
     res.setHeader("Set-Cookie", [
       cookie.serialize("username_ck", username, {
@@ -171,36 +200,6 @@ function setAuthCookie(res, token, username, userId, role) {
       })
     ]);
 
-  /* } else {
-
-    res.setHeader("Set-Cookie", [
-      cookie.serialize("username_ck", username, {
-        path: "/",
-        sameSite: true,
-        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
-        maxAge: COOKIE_EXPIRES_MS / 1000,
-      }),
-      cookie.serialize("userId_ck", userId, {
-        path: "/",
-        sameSite: true,
-        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
-        maxAge: COOKIE_EXPIRES_MS / 1000,
-      }),
-      cookie.serialize("role_ck", role, {
-        path: "/",
-        sameSite: true,
-        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
-        maxAge: COOKIE_EXPIRES_MS / 1000,
-      }),
-      cookie.serialize("auth_ck", token, {
-        path: "/",
-        httpOnly: true,
-        sameSite: true,
-        expires: new Date(Date.now() + COOKIE_EXPIRES_MS),
-        maxAge: COOKIE_EXPIRES_MS / 1000,
-      })
-    ]);
-
-  } */
+  }
 }
 exports.setAuthCookie = setAuthCookie;
