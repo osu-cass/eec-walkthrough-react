e1ae817ad60fda76bd3d16ed2bec27208cf0e093
silverware13 <thomasza@oregonstate.edu>
2020-09-18 00:51:05 -0700

    Improve rich text toolbar

--- client/src/components/General/RichTextEditor.js
+++ client/src/components/General/RichTextEditor.js
@@ -6,11 +6,28 @@ import "react-quill/dist/quill.snow.css";
 // A textarea that supports underline, bold, and italic text
 function RichTextEditor(props) {
 
+  const modules = {
+    toolbar: [
+      ["bold", "italic", "underline", "strike"],
+      [{"list": "ordered"}, {"list": "bullet"}],
+      ["link"],
+      ["clean"]
+    ],
+  };
+ 
+  const formats = [
+    "bold", "italic", "underline", "strike",
+    "list", "bullet",
+    "link"
+  ];
+
   return (
     <ReactQuill
       value={props.value}
       onChange={(text) => props.onChange(text)}
       id={`quill-${props.id}`}
+      modules={modules}
+      formats={formats}
     />
   );
 
