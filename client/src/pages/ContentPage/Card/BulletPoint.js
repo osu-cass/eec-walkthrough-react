359229b779cc394106ff4f7d9f25e27689200e05
silverware13 <thomasza@oregonstate.edu>
2020-09-18 20:05:41 -0700

    Add inline support for images

--- client/src/pages/ContentPage/Card/BulletPoint.js
+++ client/src/pages/ContentPage/Card/BulletPoint.js
@@ -90,17 +90,31 @@ function BulletPoint (props) {
       )}
 
       {getContentType(props.text, props.label, props.url) === 2 ? (
-        <div className={`row mx-auto ${props.highlightStyle === 1 ? "new-review-item" : ""} ${props.internal ? "internal-item" : ""}
+        <div className={`${props.inline ? "inline-graphic align-top" : "row mx-auto"} ${props.highlightStyle === 1 ? "new-review-item" : ""} ${props.internal ? "internal-item" : ""}
           ${props.highlightStyle === 2 ? "move-review-item" : ""} ${props.highlightStyle === 3 ? "old-review-item" : ""}`}
         >
-          <div className="icon-td pb-2">
-            <i className={`fas fa-fw fa-${props.icon} mr-2 icon-item ${styleText(props.icon)} indent-level-${props.indentation}`}
-              style={{color: props.color}}
-              title={props.tooltip}
-            />
-          </div>
-          <div className="content-td pb-2 col">
+
+          {props.inline ? (
+            null
+          ) : (
+            <div className="icon-td pb-2">
+              <i className={`fas fa-fw fa-${props.icon} mr-2 icon-item ${styleText(props.icon)} indent-level-${props.indentation}`}
+                style={{color: props.color}}
+                title={props.tooltip}
+              />
+            </div>
+          )}
+
+          <div className={`${props.inline ? "mr-3" : "col"} content-td pb-2`}>
             <div className="pb-1">
+              {props.inline ? (
+                <i className={`d-inline fas fa-fw fa-${props.icon} mr-2 icon-item ${styleText(props.icon)} indent-level-${props.indentation}`}
+                  style={{color: props.color}}
+                  title={props.tooltip}
+                />
+              ) : (
+                null
+              )}
               <span className={`icon-item-text ${styleText(props.icon) || isBold(props.bold)}`}>
                 {props.text}
               </span>
