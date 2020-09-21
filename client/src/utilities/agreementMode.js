da813d25d39d8b4aff996e645cec21698f979a24
silverware13 <thomasza@oregonstate.edu>
2020-09-20 23:58:38 -0700

    Require the image upload agreement to be re-accepted every twelve hours

--- client/src/utilities/agreementMode.js
+++ client/src/utilities/agreementMode.js
@@ -2,20 +2,39 @@
 // Description: uses local storage to track the current agreements
 // that the user has accepted
 
-// Gets the current public mode value.
-// If the user roles is less than editor, always return public mode.
+// gets the current agreement mode value
 export function getAgreement(name) {
-
-  const mode = window.localStorage.getItem(`agreement-${name}`);
-  if (mode === "true") {
-    return true;
-  } else {
+  const agreementString = window.localStorage.getItem(`agreement-store-${name}`);
+  
+  // see if there is any value in storage
+  if (!agreementString) {
     return false;
   }
 
+  const agreement = JSON.parse(agreementString);
+  const currentTime = new Date();
+
+  // compare the expiry time of the agreement with the current time
+  if (currentTime.getTime() > agreement.expiry) {
+    window.localStorage.removeItem(`agreement-store-${name}`);
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // accept agreement
 export function acceptAgreement(name) {
-  window.localStorage.setItem(`agreement-${name}`, "true");
+  // this is how long we will wait until asking for a new agreement
+  const expirationLimit = 12 * 60 * 60 * 1000; // 12 hours
+  const currentTime = new Date();
+
+  // save the time that the agreement was made
+  const agreement = {
+    value: "true",
+    expiry: currentTime.getTime() + expirationLimit
+  }
+
+  // store the agreement
+  window.localStorage.setItem(`agreement-store-${name}`, JSON.stringify(agreement));
 }
