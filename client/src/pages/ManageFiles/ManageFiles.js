032b8ee4074353f9ff0d49d89bea49525f3e3fde
silverware13 <thomasza@oregonstate.edu>
2020-09-24 15:59:13 -0700

    Show a generic message when visiting a user who has not uploaded any images in the image management page

--- client/src/pages/ManageFiles/ManageFiles.js
+++ client/src/pages/ManageFiles/ManageFiles.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, Fragment} from "react";
 import LoadingOverlay from "../../components/General/LoadingOverlay";
 import {API_URL} from "../../utilities/constants";
 import Sanitized from "../../components/General/Sanitized";
@@ -113,86 +113,96 @@ function ManageFiles() {
 
       <LoadingOverlay loading={loading} />
 
-      <table className="file-table shadow">
-        <thead>
-          <tr>
-            <th className="pl-5" style={{width: "15%"}}>
-              Image
-            </th>
-            {searchFields.sortValue === 0 ? (
-              <th className="active-sort" style={{width: "30%"}} onClick={() => changeSort(0, true)}>
-                File Name <small>{searchFields.orderValue ? "▲" : "▼" }</small>
-              </th>
-            ) : (
-              <th style={{width: "30%"}} onClick={() => changeSort(0, false)}>
-                File Name <small>▼</small>
-              </th>
-            )}
-            {searchFields.sortValue === 1 ? (
-              <th className="active-sort" style={{width: "30%"}} onClick={() => changeSort(1, true)}>
-                Source <small>{searchFields.orderValue ? "▲" : "▼" }</small>
-              </th>
-            ) : (
-              <th style={{width: "30%"}} onClick={() => changeSort(1, false)}>
-                Source <small>▼</small>
-              </th>
-            )}
-            {searchFields.sortValue === 2 ? (
-              <th className="active-sort" style={{width: "5%"}} onClick={() => changeSort(2, true)}>
-                Used on Website <small>{searchFields.orderValue ? "▲" : "▼" }</small>
-              </th>
-            ) : (
-              <th style={{width: "5%"}} onClick={() => changeSort(2, false)}>
-                Used on Website <small>▼</small>
-              </th>
-            )}
-            <th style={{width: "5%"}}>
-              User ID 
-            </th>
-            <th style={{width: "15%"}}>
-              Delete File
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {files.map((file) =>
-            <tr key={file.userId + file.name}>
-              <td className="file-data pl-5 align-top">
-                <Image
-                  url={file.url}
-                  title={file.name}
-                  thumbnail={false}
-                  header={true}
-                />
-              </td>
-              <td className="file-data align-top">
-                {file.name}
-              </td>
-              <td className="file-data-source align-top">
-                <Sanitized html={file.source} />
-              </td>
-              <td className="file-data align-top">
-                {file.used}
-              </td>
-              <td className="file-data align-top">
-                {file.userId}
-              </td>
-              <td className="file-data text-left align-top">
-                <Button className="mx-1" variant="danger" onClick={() => deleteFile(file.name)}>
+      {files.length ? (
+        <Fragment>
+          <table className="file-table shadow">
+            <thead>
+              <tr>
+                <th className="pl-5" style={{width: "15%"}}>
+                  Image
+                </th>
+                {searchFields.sortValue === 0 ? (
+                  <th className="active-sort" style={{width: "30%"}} onClick={() => changeSort(0, true)}>
+                    File Name <small>{searchFields.orderValue ? "▲" : "▼" }</small>
+                  </th>
+                ) : (
+                  <th style={{width: "30%"}} onClick={() => changeSort(0, false)}>
+                    File Name <small>▼</small>
+                  </th>
+                )}
+                {searchFields.sortValue === 1 ? (
+                  <th className="active-sort" style={{width: "30%"}} onClick={() => changeSort(1, true)}>
+                    Source <small>{searchFields.orderValue ? "▲" : "▼" }</small>
+                  </th>
+                ) : (
+                  <th style={{width: "30%"}} onClick={() => changeSort(1, false)}>
+                    Source <small>▼</small>
+                  </th>
+                )}
+                {searchFields.sortValue === 2 ? (
+                  <th className="active-sort" style={{width: "5%"}} onClick={() => changeSort(2, true)}>
+                    Used on Website <small>{searchFields.orderValue ? "▲" : "▼" }</small>
+                  </th>
+                ) : (
+                  <th style={{width: "5%"}} onClick={() => changeSort(2, false)}>
+                    Used on Website <small>▼</small>
+                  </th>
+                )}
+                <th style={{width: "5%"}}>
+                  User ID 
+                </th>
+                <th style={{width: "15%"}}>
                   Delete File
-                </Button>
-              </td>
-            </tr>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {files.map((file) =>
+                <tr key={file.userId + file.name}>
+                  <td className="file-data pl-5 align-top">
+                    <Image
+                      url={file.url}
+                      title={file.name}
+                      thumbnail={false}
+                      header={true}
+                    />
+                  </td>
+                  <td className="file-data align-top">
+                    {file.name}
+                  </td>
+                  <td className="file-data-source align-top">
+                    <Sanitized html={file.source} />
+                  </td>
+                  <td className="file-data align-top">
+                    {file.used}
+                  </td>
+                  <td className="file-data align-top">
+                    {file.userId}
+                  </td>
+                  <td className="file-data text-left align-top">
+                    <Button className="mx-1" variant="danger" onClick={() => deleteFile(file.name)}>
+                      Delete File
+                    </Button>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+          {cursor === "null" ? (
+            null
+          ) : (
+            <LoadMoreButton
+              onUpdate={() => handleLoadMore(cursor)}
+              loading={loading} 
+            />
           )}
-        </tbody>
-      </table>
-      {cursor === "null" ? (
-        null
+        </Fragment>
       ) : (
-        <LoadMoreButton
-          onUpdate={() => handleLoadMore(cursor)}
-          loading={loading} 
-        />
+        <div className="table-container">
+          <div className="prompt-container my-3 py-5 bg-white card rounded shadow-sm">
+              <h3 className="py-5 font-weight-bold">This user has not uploaded any images</h3>
+          </div>
+        </div>
       )}
     </div>
   );
