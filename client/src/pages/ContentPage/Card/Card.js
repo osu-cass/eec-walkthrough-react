71b8e284dbf9000d8f08d9bca095899769dfedd2
silverware13 <thomasza@oregonstate.edu>
2020-10-05 12:02:38 -0700

    Improve comments for the card component

--- client/src/pages/ContentPage/Card/Card.js
+++ client/src/pages/ContentPage/Card/Card.js
@@ -9,7 +9,7 @@ import OrderObjectButton from "../Various/OrderObjectButton";
 import PropTypes from "prop-types";
 import "./Card.css";
 
-// A single card on a subject or industry page
+// A single card that belongs to a header
 function Card(props) {
 
   const [imageItems, setImageItems] = useState([]);
@@ -38,7 +38,6 @@ function Card(props) {
 
   // Get information about the current card type and the correct set of items
   useEffect(() => {
-
     // Returns information about the correct array of items to use
     function getItemInfo() {
 
@@ -100,6 +99,7 @@ function Card(props) {
     }
   }
 
+  // Checks if the current card should be displayed
   return (!props.card.approved && props.mode !== 1 && (props.mode !== 2 || props.publishedMode !== 0)) || (props.publicMode === 1 && isInternal() && props.mode === 0) ? (
     null
   ) : (
@@ -120,12 +120,16 @@ function Card(props) {
           aria-controls={"collapse" + props.card.cardId}
           className="col pl-0 pr-0"
         >
+
+          {/* The title that is displayed at the top of the card */}
           <span className="align-middle">
             {(props.mode === 1 || (props.mode === 2 && props.publishedMode === 0)) && props.card.tempCardId ? (props.card.tempTitle) : (props.card.title)}
           </span>
+
         </div>
         {props.mode === 1 ? (
           <div className="row ml-auto mr-0">
+            {/* Button for editing the current card */}
             <EditCard
               card={props.unfilteredCard}
               handleUpdate={(object, type, action) => props.handleUpdate(object, type, action)}
@@ -134,6 +138,8 @@ function Card(props) {
               sources={props.sources}
               cardTitles={props.cardTitles}
             />
+
+            {/* Used to compare changes made to the card with the previous version */}
             <ReviewCard
               handleUpdate={(object, type, action) => props.handleUpdate(object, type, action)}
               edited={props.card.edited}
@@ -143,8 +149,10 @@ function Card(props) {
           </div>
         ) : (
           <Fragment>
+            {/* Only display the buttons for reordering cards in move mode */}
             {props.mode === 2 ? (
               <div className="row ml-auto mr-0">
+                {/* Button to move card up */}
                 <OrderObjectButton
                   up={true}
                   header={false}
@@ -154,6 +162,7 @@ function Card(props) {
                   approved={props.card.approved}
                   publishedMode={props.publishedMode}
                 />
+                {/* Button to move card down */}
                 <OrderObjectButton
                   up={false}
                   header={false}
@@ -172,6 +181,9 @@ function Card(props) {
       </CardBS.Header>
       <div id={"collapse" + props.card.cardId} className="collapse show" aria-labelledby={"heading" + props.card.cardId}>
         <CardBS.Body className="content-card-body">
+
+          {/* Special content to show that a card with no content is invalid */}
+          {/* There should be no way to create an invalid card, so the presence of one shows that there is a bug */}
           {props.card.invalid ? (
             <Fragment>
               <h4>INVALID CARD!</h4>
@@ -183,32 +195,39 @@ function Card(props) {
           ) : (
             null
           )}
+
+          {/* Based on the card type we will display the items in a different way */}
+          {/* This is where we display images as rows of resized thumbnails */}
           {cardType === 1 || cardType === 11 ? (
             <ThumbnailGallery items={items} />
           ) : (
-            <Fragment>
-              {cardType === 2 || cardType === 12 ? (
-                <BasicItems
-                  items={items}
-                  mode={props.mode}
-                  publicMode={props.publicMode}
-                  handleTimestamp={(m, a, i) => props.handleTimestamp(m, a, i, props.card.cardId)}
-                  reviewing={false}
-                  setCheck={(check, itemId) => props.setCheck(check, itemId, props.card.cardId)}
-                  expandableList={true}
-                />
-              ) : (
-                <BasicItems
-                  items={items}
-                  mode={props.mode}
-                  publicMode={props.publicMode}
-                  handleTimestamp={(m, a, i) => props.handleTimestamp(m, a, i, props.card.cardId)}
-                  reviewing={false}
-                  setCheck={(check, itemId) => props.setCheck(check, itemId, props.card.cardId)}
-                />
-              )}
-            </Fragment>
+            null
           )}
+
+          {/* Based on the card type we will display the items in a different way */}
+          {/* The first option adds a button for displaying the content in parts */}
+          {/* The second option is default and is a bulleted list of items */}
+          {cardType === 2 || cardType === 12 ? (
+            <BasicItems
+              items={items}
+              mode={props.mode}
+              publicMode={props.publicMode}
+              handleTimestamp={(m, a, i) => props.handleTimestamp(m, a, i, props.card.cardId)}
+              reviewing={false}
+              setCheck={(check, itemId) => props.setCheck(check, itemId, props.card.cardId)}
+              expandableList={true}
+            />
+          ) : (
+            <BasicItems
+              items={items}
+              mode={props.mode}
+              publicMode={props.publicMode}
+              handleTimestamp={(m, a, i) => props.handleTimestamp(m, a, i, props.card.cardId)}
+              reviewing={false}
+              setCheck={(check, itemId) => props.setCheck(check, itemId, props.card.cardId)}
+            />
+          )}
+
         </CardBS.Body>
       </div>
     </CardBS>
