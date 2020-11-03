8728040b94b965026c72b8a5ad7849a835a40a56
silverware13 <thomasza@oregonstate.edu>
2020-11-02 20:41:25 -0800

    Add interactive sponsors to the homepage

---
+++ client/src/pages/HomePage/Sponsor.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+import PropTypes from "prop-types";
+import "./Sponsor.css";
+
+// An interactive sponsor image and link
+function Sponsor(props) {
+
+  return (
+      <div className="sponsor-container mx-2 my-5">
+        <a href={props.websiteUrl}>
+          <img
+            src={props.imageUrl}
+            alt={props.name}
+            title={props.name}
+            className="sponsor-img img-fluid img-thumbnail px-3 py-3"
+          />
+        </a>
+      </div>
+  );
+}
+export default Sponsor;
+
+Sponsor.propTypes = {
+  name: PropTypes.string,
+  imageUrl: PropTypes.string,
+  websiteUrl: PropTypes.string
+};
