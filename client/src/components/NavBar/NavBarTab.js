28d44d3f9c15af081368b3f23a2da14133936b8a
silverware13 <thomasza@oregonstate.edu>
2020-11-09 15:25:01 -0800

    Add special image dropdown rows to the navbar

--- client/src/components/NavBar/NavBarTab.js
+++ client/src/components/NavBar/NavBarTab.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import PropTypes from "prop-types";
 import CreatePage from "../Sidebar/CreatePage";
 import EditCategory from "../Sidebar/EditCategory";
@@ -8,6 +8,35 @@ import "./NavBar.css";
 function NavBarTab (props) {
 
   const [subShow, setSubShow] = useState(false);
+  const [categoryGroups, setCategoryGroups] = useState([]);
+
+  // Store category pages in groups of three, so that we can easily make rows
+  useEffect(() => {
+    const groups = [];
+    if (props.category.pages.length) {
+
+      let counter = 0;
+
+      // create a group for each three pages
+      for (let i = counter; counter < props.category.pages.length; i++) {
+        const newGroup = {
+          id: i,
+          pages: []
+        };
+        // save up to three pages into the new group
+        for (let i = counter; i < props.category.pages.length && i < counter + 3; i++) {
+          newGroup.pages.push(props.category.pages[i]);
+        }
+        counter += 3;
+        groups.push(newGroup);
+      }
+
+      setCategoryGroups(groups);
+
+    } else {
+      setCategoryGroups([]);
+    }
+  }, [props.category])
 
   return (
     props.visibleTabs[props.category.categoryId] || (props.subTab && !props.visibleTabs[props.category.categoryId]) ? (
@@ -39,47 +68,106 @@ function NavBarTab (props) {
           </div>
         </a>
 
-        <div className={`${props.subTab ? "sub-dropdown-content dropdown-menu-right" : "dropdown-content"} ${props.subTab && subShow ? "sub-show" : ""}`} >
-          {/* Pages */}
-          {props.category.pages.map((page) =>
-            <a
-              href={`/wiki/${props.category.pluralName.replace(/\s+/g, "-").toLowerCase()}/${page.pageId}`}
-              key={page.pageId}
-            >
-              <div className="navbar-item px-2 py-1">
-                {page.name}
-                {page.approved === 0 ? (
-                  <span>&nbsp;<i className="sidebar-icons fas fa-fw fa-wrench fa-sm ml-1" /></span>
-                ) : (
-                  null
-                )}
-                {page.internal ? (
-                  <span>&nbsp;<i className="sidebar-icons fas fa-fw fa-unlock-alt fa-sm ml-1" /></span>
-                ) : (
-                  null
+        {/* Include images if this isn't extended from a 'more' tab */}
+        {props.subTab ? (
+
+          <div className={`sub-dropdown-content dropdown-menu-right" ${subShow ? "sub-show" : ""}`} >
+
+            {/* Pages */}
+            {props.category.pages.map((page) =>
+              <a
+                href={`/wiki/${props.category.pluralName.replace(/\s+/g, "-").toLowerCase()}/${page.pageId}`}
+                key={page.pageId}
+              >
+                <div className="navbar-item px-2 py-1">
+                  {page.name}
+                  {page.approved === 0 ? (
+                    <span>&nbsp;<i className="sidebar-icons fas fa-fw fa-wrench fa-sm ml-1" /></span>
+                  ) : (
+                    null
+                  )}
+                  {page.internal ? (
+                    <span>&nbsp;<i className="sidebar-icons fas fa-fw fa-unlock-alt fa-sm ml-1" /></span>
+                  ) : (
+                    null
+                  )}
+                </div>
+              </a>
+            )}
+
+            {/* Create page button */}
+            <CreatePage
+              navbar={true}
+              title={`Create ${props.category.pluralName} Page`}
+              collectionLink={`wiki/${props.category.pluralName.replace(/\s+/g, "-").toLowerCase()}`}
+              refresh={() => props.fetchData()}
+              role={props.role}
+              categoryId={props.category.categoryId}
+            />
+
+            {/* Edit category button */}
+            <EditCategory
+              navbar={true}
+              refresh={() => props.fetchData()}
+              role={props.role}
+              category={props.category}
+            />
+          </div>
+
+        ) : (
+
+          <div className={`dropdown-content dropdown-size-${props.category.pages.length > 3 ? "3" : props.category.pages.length}`} >
+
+            {/* Pages */}
+            {categoryGroups.map((row) =>
+              <div className="row align-items-end px-0 mx-0" key={row.id}>
+                {row.pages.map((page) =>
+                  <div className="col dropdown-nav-col px-0 mx-0" key={page.pageId}>
+                    <a
+                      href={`/wiki/${props.category.pluralName.replace(/\s+/g, "-").toLowerCase()}/${page.pageId}`}
+                    >
+                      <div className="navbar-item navbar-image-item text-center pt-auto px-2 py-1">
+                        <img src={page.imageUrl} alt={page.name} className="cat-tag-img mb-2" />
+                        <br />
+                        {page.name}
+                        {page.approved === 0 ? (
+                          <span>&nbsp;<i className="sidebar-icons fas fa-fw fa-wrench fa-sm ml-1" /></span>
+                        ) : (
+                          null
+                        )}
+                        {page.internal ? (
+                          <span>&nbsp;<i className="sidebar-icons fas fa-fw fa-unlock-alt fa-sm ml-1" /></span>
+                        ) : (
+                          null
+                        )}
+                      </div>
+                    </a>
+                  </div>
                 )}
               </div>
-            </a>
-          )}
+            )}
+          
 
-          {/* Create page button */}
-          <CreatePage
-            navbar={true}
-            title={`Create ${props.category.pluralName} Page`}
-            collectionLink={`wiki/${props.category.pluralName.replace(/\s+/g, "-").toLowerCase()}`}
-            refresh={() => props.fetchData()}
-            role={props.role}
-            categoryId={props.category.categoryId}
-          />
+            {/* Create page button */}
+            <CreatePage
+              navbar={true}
+              title={`Create ${props.category.pluralName} Page`}
+              collectionLink={`wiki/${props.category.pluralName.replace(/\s+/g, "-").toLowerCase()}`}
+              refresh={() => props.fetchData()}
+              role={props.role}
+              categoryId={props.category.categoryId}
+            />
 
-          {/* Edit category button */}
-          <EditCategory
-            navbar={true}
-            refresh={() => props.fetchData()}
-            role={props.role}
-            category={props.category}
-          />
-        </div>
+            {/* Edit category button */}
+            <EditCategory
+              navbar={true}
+              refresh={() => props.fetchData()}
+              role={props.role}
+              category={props.category}
+            />
+          </div>
+
+        )}
 
       </div>
     ) : (
