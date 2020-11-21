09f941a1c85e3531be68d0483e67ad0fbd2f1398
silverware13 <thomasza@oregonstate.edu>
2020-11-21 12:52:45 -0800

    Allow admins to upload or select new banner images for the homepage

---
+++ models/banners.js
@@ -0,0 +1,59 @@
+// File: banners.js
+// Description: Provides functions for working with banners
+
+const {pool} = require("../services/database/mysqlPool");
+
+
+// returns all of the contributors
+async function getBanners() {
+
+  try {
+
+    // get all banners
+    const sql = "SELECT * FROM Banners ORDER BY bannerId;";
+    const results = await pool.query(sql, []);
+
+    const finalResults = {
+      banners: results[0]
+    };
+
+    return finalResults;
+
+  } catch (err) {
+    console.error("Error getting banners");
+    throw Error(err);
+  }
+
+}
+exports.getBanners = getBanners;
+
+
+// create banners
+async function createBanners(banners) {
+
+  try {
+
+    // delete all old banners
+    let sql = "DELETE FROM Banners;";
+    let results = await pool.query(sql, []);
+
+    // create all new banners
+    for (let i = 0; i < banners.length; i++) {
+      sql = "INSERT INTO Banners (imageUrl) " +
+      "VALUES (?);";
+      await pool.query(sql, banners[i]);
+    }
+
+    const finalResults = {
+      bannersCreated: 1
+    };
+
+    return finalResults;
+
+  } catch (err) {
+    console.error("Error creating banners");
+    throw Error(err);
+  }
+
+}
+exports.createBanners = createBanners;
---
+++ routes/banners.js
@@ -0,0 +1,72 @@
+// File: banners.js
+// Description: handles routing for banners
+
+const express = require("express");
+const app = express.Router();
+const {validationResult} = require("express-validator");
+const {
+  getBanners,
+  createBanners
+} = require("../models/banners");
+const {
+  postBannerVal
+} = require("../services/validation/requestValidation");
+const {
+  requireAuth,
+  roleCheck
+} = require("../services/authentication/cookieAuth");
+
+
+// get all banners
+app.get("/", async (req, res) => {
+
+  try {
+    console.log("View banners");
+
+    // get all banners
+    const results = await getBanners();
+    res.status(200).send(results);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({error: "An internal server error occurred. Please try again later."});
+  }
+
+});
+
+
+// post banner data
+app.post("/", requireAuth, postBannerVal.validation, async (req, res) => {
+
+  try {
+
+    console.log("Create banners");
+
+    const banners = req.body.banners;
+
+    // confirm that the request is valid
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.error(errors.array());
+      return res.status(422).json({errors: errors.array()});
+    }
+
+    // make sure the user is allowed to perform this action
+    if (!await roleCheck(5, req.auth.userId)) {
+      res.status(401).send({error: "Unauthorized user attempting to create banners."});
+      return;
+    }
+
+    // create banners
+    const results = await createBanners(banners);
+    res.status(200).send(results);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({error: "An internal server error occurred. Please try again later."});
+  }
+
+});
+
+
+module.exports = app;
