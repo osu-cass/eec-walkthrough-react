2190f205920ea44c53633f92c1e273cd89637733
Silverware <silverware13@gmail.com>
2020-12-07 14:43:56 -0800

    Add route for edit quiz page

--- client/src/pages/ContentPage/Various/QuizButton.js
+++ client/src/pages/ContentPage/Various/QuizButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 import PropTypes from "prop-types";
 import {NavLink} from "react-router-dom";
 
@@ -44,6 +44,7 @@ function QuizButton(props) {
           </div>
         )}
 
+        {/* Previous attempt score */}
         {props.quizScore >= 0 ? (
           <h4 className="text-center mb-4">
             You previously got {props.quizScore}% of questions correct
@@ -55,7 +56,43 @@ function QuizButton(props) {
       </div>
     </div>
   ) : (
-    null
+    <Fragment>
+      {/* If we are in edit mode and we are an admin allow quiz editing */}
+      {props.role === 5 && props.mode === 1 ? (
+
+        <div className="quiz-button-container">
+
+          {/* Quiz header */}
+          <div className={`d-flex reference-header-bar justify-content-between sticky-top
+            my-3 p-3 text-dark-50 rounded shadow-sm border`} style={{top: "1em", zIndex: "998"}}
+          >
+            <div className="row w-100 ml-0">
+              <h4 className="flex-grow-1 font-weight-bold my-0 mx-0">
+                Quiz
+              </h4>
+            </div>
+          </div>
+
+          {/* Card describing quiz */}
+          <div className="my-3 p-3 card rounded shadow-sm">
+            <span className="font-weight-bold text-center mt-4 mb-2">
+              Test your knowledge by taking the {props.pageName} quiz
+            </span>
+
+            {/* Quiz button */}
+            <div className="text-center my-4">
+              <NavLink className="quiz-nav-link" to={`/edit-quiz/${props.pageId}`}>
+                <button className="btn btn-lg btn-info d-print-none">
+                  Edit the Quiz
+                </button>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      ) : (
+        null
+      )}
+    </Fragment>
   );
 
 }
---
+++ client/src/pages/QuizEdit/QuizEdit.js
@@ -0,0 +1,138 @@
+import React, {useState, useEffect, Fragment} from "react";
+import LoadingOverlay from "../../components/General/LoadingOverlay";
+import {API_URL} from "../../utilities/constants";
+import {useParams, useHistory} from "react-router-dom";
+import Error404 from "../404/Error404";
+import Error500 from "../500/Error500";
+import QuestionEdit from "./QuestionEdit";
+import Error from "../../components/General/Error";
+import "./QuizEdit.css";
+
+// Page for editing quizzes
+function QuizEdit() {
+
+  const [title, setTitle] = useState("");
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+  const {pageId} = useParams();
+  const history = useHistory();
+
+  // Gets quiz info when the page first loads
+  useEffect(() => {
+    // abort controller for if this component is cleaned up before
+    // the fetch request gets a response
+    let ignore = false;
+    const controller = new AbortController();
+
+    async function fetchData() {
+      try {
+
+        // Fetch quiz info
+        const results = await fetch(`${API_URL}/quizzes/${pageId}`, {
+          signal: controller.signal,
+          method: "GET",
+          credentials: "include",
+          headers: {"Content-Type": "application/json"}
+        });
+
+        // if this component is cleaned up, stop here
+        if (ignore) {
+          return;
+        }
+
+        if (results.ok) {
+          const obj = await results.json();
+          setTitle(obj.title);
+          setQuestions(obj.questions);
+        } else {
+          const obj = await results.json();
+
+          if (results.status === 404) {
+            console.error("Couldn't find quiz");
+            setError(1);
+          } else if (results.status === 500 || typeof obj.error === "undefined") {
+            console.error("An internal server error occurred while trying to move the card.");
+            setError(2);
+          } else {
+            console.error(obj.error);
+            setError(3);
+          }
+        }
+
+        setLoading(false);
+
+      } catch (err) {
+        if (err instanceof DOMException) {
+          if (process.env.NODE_ENV === "development") {
+            console.log("HTTP request aborted");
+          }
+        } else {
+          throw err;
+        }
+      }
+    }
+
+    fetchData();
+
+    // clean up function
+    return () => {
+      ignore = true;
+      controller.abort();
+    };
+    // eslint-disable-next-line
+  }, [pageId]);
+
+  // save the quiz settings
+  async function saveQuiz() {
+    history.push(`/wiki/search-results/${pageId}`);
+  }
+
+  return !error ? (
+    <div className="container quiz-page-container my-5">
+
+      <LoadingOverlay loading={loading} />
+
+      <div className="d-flex header-bar justify-content-between mt-3 mb-4 p-3 text-dark-50 rounded shadow-sm border generic-header-bar">
+        <div className="row mx-2">
+          <h4 className="flex-grow-1 font-weight-bold">
+            {title} Quiz
+          </h4>
+        </div>
+      </div>
+
+      {questions.map((question) =>
+        <QuestionEdit
+          key={question.questionId}
+          questionId={question.questionId}
+          text={question.text}
+          answers={question.answers}
+          type={question.type}
+          imageUrl={question.imageUrl}
+        />
+      )}
+
+      <Error message={errorMessage} />
+
+      <div className="mt-4 submit-quiz-box">
+        <button className="submit-quiz btn btn-lg btn-success pull-right" onClick={() => saveQuiz()}>
+          Save Quiz
+        </button>
+      </div>
+
+    </div>
+
+  ) : (
+
+    <Fragment>
+      {/* There was an error while attempting to load the page */}
+      {error === 1 ? (
+        <Error404 />
+      ) : (
+        <Error500 />
+      )}
+    </Fragment>
+  );
+}
+export default QuizEdit;
