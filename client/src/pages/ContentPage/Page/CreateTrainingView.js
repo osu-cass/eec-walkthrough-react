2a34c13c950567656a5cf30d8a26c42e2eda71fd
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-04-01 12:47:19 -0700

    added training mode button

---
+++ client/src/pages/ContentPage/Page/CreateTrainingView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CreateTrainingView({ role, mode }) {
+	return (
+		<div>
+			<p>
+				role = {role}, mode = {mode}
+			</p>
+		</div>
+	)
+}
+
+export default CreateTrainingView
