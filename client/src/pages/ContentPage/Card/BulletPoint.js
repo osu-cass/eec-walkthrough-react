623c4d18be35fcafbdfee731d04b20fdba111da8
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-04-04 00:40:30 -0700

    added select icon and grayed out bulletPoint in training mode. Experimenting gray out on BulletPointItem level

--- client/src/pages/ContentPage/Card/BulletPoint.js
+++ client/src/pages/ContentPage/Card/BulletPoint.js
@@ -1,128 +1,129 @@
-import React, {Fragment} from "react";
-import PropTypes from "prop-types";
-import BulletPointItem from "./BulletPointItem";
-import BulletPointGraphic from "./BulletPointGraphic";
-import BulletPointResource from "./BulletPointResource";
-import BulletPointText from "./BulletPointText";
-import "./BulletPoint.css";
+import React, { Fragment, useState } from 'react'
+import PropTypes from 'prop-types'
+import BulletPointItem from './BulletPointItem'
+import BulletPointGraphic from './BulletPointGraphic'
+import BulletPointResource from './BulletPointResource'
+import BulletPointText from './BulletPointText'
+import './BulletPoint.css'
+import { AiOutlinePlusCircle } from 'react-icons/ai'
 
 // Represents a single bullet point inside a card
 function BulletPoint(props) {
+	// set item selected state in create training mode (mode === 3)
+	const [selected, SetSelected] = useState(false)
 
-  // Don't show bullet points that are internal when we are viewing in public mode
-  return !props.internal || !props.publicMode ? (
-    <Fragment>
-      {/* If the bullet point is an item */}
-      {props.groupIndex === 1 && props.icon !== "font" ? (
-        <BulletPointItem
-          id={props.id}
-          text={props.text}
-          icon={props.icon}
-          indentation={props.indentation}
-          mode={props.mode}
-          color={props.color}
-          tooltip={props.tooltip}
-          setCheck={(state, id) => props.setCheck(state, id)}
-          checked={props.checked}
-          highlightStyle={props.highlightStyle}
-          internal={props.internal}
-          source={props.source}
-          sourceText={props.sourceText}
-          inline={props.inline}
-        />
-      ) : (
-        null
-      )}
-
-      {/* If the bullet point is a text field */}
-      {props.groupIndex === 1 && props.icon === "font" ? (
-        <BulletPointText
-          id={props.id}
-          text={props.text}
-          mode={props.mode}
-          indentation={props.indentation}
-          highlightStyle={props.highlightStyle}
-          internal={props.internal}
-          inline={props.inline}
-          source={props.source}
-          sourceText={props.sourceText}
-        />
-      ) : (
-        null
-      )}
-
-      {/* If the bullet point is a graphic */}
-      {props.groupIndex === 2 ? (
-        <BulletPointGraphic
-          text={props.text}
-          label={props.label}
-          url={props.url}
-          icon={props.icon}
-          indentation={props.indentation}
-          color={props.color}
-          tooltip={props.tooltip}
-          highlightStyle={props.highlightStyle}
-          internal={props.internal}
-          source={props.source}
-          sourceText={props.sourceText}
-          inline={props.inline}
-        />
-      ) : (
-        null
-      )}
-
-      {/* If the bullet point is a resource */}
-      {props.groupIndex === 3 ? (
-        <BulletPointResource
-          id={props.id}
-          text={props.text}
-          label={props.label}
-          url={props.url}
-          icon={props.icon}
-          created={props.created}
-          indentation={props.indentation}
-          mode={props.mode}
-          contentMode={props.contentMode}
-          handleTimestamp={(m) => props.handleTimestamp(m)}
-          color={props.color}
-          tooltip={props.tooltip}
-          reviewing={props.reviewing}
-          highlightStyle={props.highlightStyle}
-          internal={props.internal}
-          inline={props.inline}
-        />
-      ) : (
-        null
-      )}
-    </Fragment>
-  ) : (
-    null
-  );
-
+	// Don't show bullet points that are internal when we are viewing in public mode
+	return !props.internal || !props.publicMode ? (
+		<div
+			className={`
+    ${props.mode === 3 && selected === false ? 'grayed-out' : ''}
+    
+    `}
+		>
+			{props.mode === 3 && (
+				<span className="training-select-icon">
+					<AiOutlinePlusCircle />
+				</span>
+			)}
+			<div style={{ display: 'inline-block' }}>
+				{/* If the bullet point is an item */}
+				{props.groupIndex === 1 && props.icon !== 'font' ? (
+					<BulletPointItem
+						id={props.id}
+						text={props.text}
+						icon={props.icon}
+						indentation={props.indentation}
+						mode={props.mode}
+						color={props.color}
+						tooltip={props.tooltip}
+						setCheck={(state, id) => props.setCheck(state, id)}
+						checked={props.checked}
+						highlightStyle={props.highlightStyle}
+						internal={props.internal}
+						source={props.source}
+						sourceText={props.sourceText}
+						inline={props.inline}
+					/>
+				) : null}
+				{/* If the bullet point is a text field */}
+				{props.groupIndex === 1 && props.icon === 'font' ? (
+					<BulletPointText
+						id={props.id}
+						text={props.text}
+						mode={props.mode}
+						indentation={props.indentation}
+						highlightStyle={props.highlightStyle}
+						internal={props.internal}
+						inline={props.inline}
+						source={props.source}
+						sourceText={props.sourceText}
+					/>
+				) : null}
+				{/* If the bullet point is a graphic */}
+				{props.groupIndex === 2 ? (
+					<BulletPointGraphic
+						text={props.text}
+						label={props.label}
+						url={props.url}
+						icon={props.icon}
+						indentation={props.indentation}
+						color={props.color}
+						tooltip={props.tooltip}
+						highlightStyle={props.highlightStyle}
+						internal={props.internal}
+						source={props.source}
+						sourceText={props.sourceText}
+						inline={props.inline}
+					/>
+				) : null}
+				{/* If the bullet point is a resource */}
+				{props.groupIndex === 3 ? (
+					<BulletPointResource
+						id={props.id}
+						text={props.text}
+						label={props.label}
+						url={props.url}
+						icon={props.icon}
+						created={props.created}
+						indentation={props.indentation}
+						mode={props.mode}
+						contentMode={props.contentMode}
+						handleTimestamp={m => props.handleTimestamp(m)}
+						color={props.color}
+						tooltip={props.tooltip}
+						reviewing={props.reviewing}
+						highlightStyle={props.highlightStyle}
+						internal={props.internal}
+						inline={props.inline}
+					/>
+				) : null}
+			</div>
+		</div>
+	) : null
 }
-export default BulletPoint;
+export default BulletPoint
 
 BulletPoint.propTypes = {
-  id: PropTypes.number,
-  text: PropTypes.string,
-  label: PropTypes.string,
-  url: PropTypes.string,
-  icon: PropTypes.string,
-  created: PropTypes.string,
-  indentation: PropTypes.number,
-  mode: PropTypes.number,
-  publicMode: PropTypes.number,
-  contentMode: PropTypes.number,
-  handleTimestamp: PropTypes.func,
-  color: PropTypes.string,
-  tooltip: PropTypes.string,
-  reviewing: PropTypes.bool,
-  setCheck: PropTypes.func,
-  checked: PropTypes.bool,
-  highlightStyle: PropTypes.number,
-  internal: PropTypes.number,
-  source: PropTypes.number,
-  sourceText: PropTypes.string,
-  inline: PropTypes.number,
-  groupIndex: PropTypes.number
-};
+	id: PropTypes.number,
+	text: PropTypes.string,
+	label: PropTypes.string,
+	url: PropTypes.string,
+	icon: PropTypes.string,
+	created: PropTypes.string,
+	indentation: PropTypes.number,
+	mode: PropTypes.number,
+	publicMode: PropTypes.number,
+	contentMode: PropTypes.number,
+	handleTimestamp: PropTypes.func,
+	color: PropTypes.string,
+	tooltip: PropTypes.string,
+	reviewing: PropTypes.bool,
+	setCheck: PropTypes.func,
+	checked: PropTypes.bool,
+	highlightStyle: PropTypes.number,
+	internal: PropTypes.number,
+	source: PropTypes.number,
+	sourceText: PropTypes.string,
+	inline: PropTypes.number,
+	groupIndex: PropTypes.number
+}
--- client/src/pages/ContentPage/Card/BulletPointItem.js
+++ client/src/pages/ContentPage/Card/BulletPointItem.js
@@ -1,157 +1,203 @@
-import React, {Fragment, useEffect, useState} from "react";
-import PropTypes from "prop-types";
-import Source from "./Source";
-import Sanitized from "../../../components/General/Sanitized";
+import React, { Fragment, useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import Source from './Source'
+import Sanitized from '../../../components/General/Sanitized'
 
 // Represents a single item type bullet inside a card
 function BulletPointItem(props) {
+	const [isMobile, setIsMobile] = useState(false)
 
-  const [isMobile, setIsMobile] = useState(false);
+	useEffect(() => {
+		if (window.innerWidth < 768) {
+			setIsMobile(true)
+		} else {
+			setIsMobile(false)
+		}
+	}, [])
 
-  useEffect(() => {
-    if (window.innerWidth < 768) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-  }, []);
+	// Sets specific styling rules based on the icon used for this item
+	function styleText(icon) {
+		if (icon === 'check-square') {
+			return 'check-square-icon'
+		}
+		if (icon === 'flag') {
+			return 'font-italic'
+		}
+		if (icon === 'angle-right') {
+			return 'opportunity-desc'
+		}
+		return ''
+	}
 
-  // Sets specific styling rules based on the icon used for this item
-  function styleText(icon) {
-    if (icon === "check-square") { return "check-square-icon"; }
-    if (icon === "flag") { return "font-italic"; }
-    if (icon === "angle-right") { return "opportunity-desc"; }
-    return "";
-  }
+	// Clicking the check icon of an opportunity item will toggle its check
+	// if it is not in move mode
+	function updateCheck(state, id) {
+		if (props.mode !== 2) {
+			props.setCheck(state, id)
+		}
+	}
+	return (
+		<Fragment>
+			{isMobile ? (
+				/* The row holding the item */
+				<div
+					className={`row mx-auto ${
+						props.highlightStyle === 1 ? 'new-review-item' : ''
+					}
+          ${props.internal ? 'internal-item' : ''} ${
+						props.highlightStyle === 2 ? 'move-review-item' : ''
+					}
+          ${props.highlightStyle === 3 ? 'old-review-item' : ''}
+          
+          `}
+				>
+					{/* Container holding item's icon */}
+					<div
+						className={`icon-td justify-content-center ${
+							props.icon === 'check-square'
+								? ''
+								: 'indent-level-' + props.indentation
+						}`}
+					>
+						{/* Check to see if the current item is a special opportunity item */}
+						{props.icon === 'check-square' ? (
+							<Fragment>
+								{/* Select the icon based on if the opportunity is checked */}
+								{props.checked && props.mode !== 2 ? (
+									<i
+										className={`fas fa-fw fa-square mr-2 icon-item indent-level-${
+											props.indentation
+										} ${styleText(props.icon)}`}
+										title={props.tooltip}
+										onClick={() => updateCheck(false, props.id)}
+									/>
+								) : (
+									<i
+										className={`fas fa-fw fa-check-square mr-2 icon-item indent-level-${
+											props.indentation
+										} ${styleText(props.icon)}`}
+										title={props.tooltip}
+										onClick={() => updateCheck(true, props.id)}
+									/>
+								)}
+							</Fragment>
+						) : (
+							<i
+								className={`fas fa-fw fa-${props.icon} mr-2 icon-item
+              ${props.icon === 'angle-right' ? 'd-none' : ''} ${styleText(
+									props.icon
+								)}`}
+								style={{ color: props.color }}
+								title={props.tooltip}
+							/>
+						)}
+					</div>
 
-  // Clicking the check icon of an opportunity item will toggle its check
-  // if it is not in move mode
-  function updateCheck(state, id) {
-    if (props.mode !== 2) {
-      props.setCheck(state, id);
-    }
-  }
+					{/* Container holding item text and possibly a source */}
+					<div className={`content-td pb-2 col`}>
+						<span className={`icon-item-text ${styleText(props.icon)}`}>
+							<Sanitized html={props.text} inline={!!props.inline} />
+						</span>
+						<Source source={props.source} sourceText={props.sourceText} />
+					</div>
+				</div>
+			) : (
+				/* The row holding the item */
+				<div
+					className={`
+            ${props.inline ? 'd-inline text-nowrap ml-1' : 'row mx-auto'} 
+            ${props.highlightStyle === 1 ? 'new-review-item' : ''}
+            ${props.internal ? 'internal-item' : ''} 
+            ${props.highlightStyle === 2 ? 'move-review-item' : ''}
+            ${props.highlightStyle === 3 ? 'old-review-item' : ''}
+          
+          `}
+				>
+					{console.log(props.mode === 3)}
+					{/* Container holding item's icon */}
+					<div
+						className={`${props.inline ? 'inline-block-icon pb-2' : ''} ${
+							props.inline && props.icon === 'circle'
+								? 'inline-circle-icon'
+								: ''
+						} icon-td
+            justify-content-center ${
+							props.icon === 'check-square'
+								? ''
+								: 'indent-level-' + props.indentation
+						}`}
+					>
+						{/* Check to see if the current item is a special opportunity item */}
+						{props.icon === 'check-square' ? (
+							<Fragment>
+								{/* Select the icon based on if the opportunity is checked */}
+								{props.checked && props.mode !== 2 ? (
+									<i
+										className={`${
+											props.inline ? 'd-inline' : ''
+										} fas fa-fw fa-square mr-2 icon-item indent-level-${
+											props.indentation
+										} ${styleText(props.icon)}`}
+										title={props.tooltip}
+										onClick={() => updateCheck(false, props.id)}
+									/>
+								) : (
+									<i
+										className={`${
+											props.inline ? 'd-inline' : ''
+										} fas fa-fw fa-check-square mr-2 icon-item indent-level-${
+											props.indentation
+										} ${styleText(props.icon)}`}
+										title={props.tooltip}
+										onClick={() => updateCheck(true, props.id)}
+									/>
+								)}
+							</Fragment>
+						) : (
+							<i
+								className={`${props.inline ? 'd-inline' : ''} fas fa-fw fa-${
+									props.icon
+								} mr-2 icon-item
+              ${props.icon === 'angle-right' ? 'd-none' : ''} ${styleText(
+									props.icon
+								)}`}
+								style={{ color: props.color }}
+								title={props.tooltip}
+							/>
+						)}
+					</div>
 
-  return (
-    <Fragment>
-
-      {isMobile ? (
-        /* The row holding the item */
-        <div className={`row mx-auto ${props.highlightStyle === 1 ? "new-review-item" : ""}
-          ${props.internal ? "internal-item" : ""} ${props.highlightStyle === 2 ? "move-review-item" : ""}
-          ${props.highlightStyle === 3 ? "old-review-item" : ""}`}
-        >
-
-          {/* Container holding item's icon */}
-          <div
-            className={`icon-td justify-content-center ${props.icon === "check-square" ? "" : "indent-level-" + props.indentation}`}
-          >
-            {/* Check to see if the current item is a special opportunity item */}
-            {props.icon === "check-square" ? (
-
-              <Fragment>
-                {/* Select the icon based on if the opportunity is checked */}
-                {props.checked && props.mode !== 2 ? (
-                  <i className={`fas fa-fw fa-square mr-2 icon-item indent-level-${props.indentation} ${styleText(props.icon)}`}
-                    title={props.tooltip}
-                    onClick={() => updateCheck(false, props.id)}
-                  />
-                ) : (
-                  <i className={`fas fa-fw fa-check-square mr-2 icon-item indent-level-${props.indentation} ${styleText(props.icon)}`}
-                    title={props.tooltip}
-                    onClick={() => updateCheck(true, props.id)}
-                  />
-                )}
-              </Fragment>
-
-            ) : (
-              <i className={`fas fa-fw fa-${props.icon} mr-2 icon-item
-              ${props.icon === "angle-right" ? "d-none" : ""} ${styleText(props.icon)}`}
-              style={{color: props.color}}
-              title={props.tooltip}
-              />
-            )}
-
-          </div>
-
-          {/* Container holding item text and possibly a source */}
-          <div className={`content-td pb-2 col`}>
-            <span className={`icon-item-text ${styleText(props.icon)}`}>
-              <Sanitized html={props.text} inline={!!props.inline} />
-            </span>
-            <Source source={props.source} sourceText={props.sourceText} />
-          </div>
-
-        </div>
-      ) : (
-        /* The row holding the item */
-        <div className={`${props.inline ? "d-inline text-nowrap ml-1" : "row mx-auto"} ${props.highlightStyle === 1 ? "new-review-item" : ""}
-          ${props.internal ? "internal-item" : ""} ${props.highlightStyle === 2 ? "move-review-item" : ""}
-          ${props.highlightStyle === 3 ? "old-review-item" : ""}`}
-        >
-
-          {/* Container holding item's icon */}
-          <div
-            className={`${props.inline ? "inline-block-icon pb-2" : ""} ${props.inline && props.icon === "circle" ? "inline-circle-icon" : ""} icon-td
-            justify-content-center ${props.icon === "check-square" ? "" : "indent-level-" + props.indentation}`}
-          >
-            {/* Check to see if the current item is a special opportunity item */}
-            {props.icon === "check-square" ? (
-
-              <Fragment>
-                {/* Select the icon based on if the opportunity is checked */}
-                {props.checked && props.mode !== 2 ? (
-                  <i className={`${props.inline ? "d-inline" : ""} fas fa-fw fa-square mr-2 icon-item indent-level-${props.indentation} ${styleText(props.icon)}`}
-                    title={props.tooltip}
-                    onClick={() => updateCheck(false, props.id)}
-                  />
-                ) : (
-                  <i className={`${props.inline ? "d-inline" : ""} fas fa-fw fa-check-square mr-2 icon-item indent-level-${props.indentation} ${styleText(props.icon)}`}
-                    title={props.tooltip}
-                    onClick={() => updateCheck(true, props.id)}
-                  />
-                )}
-              </Fragment>
-
-            ) : (
-              <i className={`${props.inline ? "d-inline" : ""} fas fa-fw fa-${props.icon} mr-2 icon-item
-              ${props.icon === "angle-right" ? "d-none" : ""} ${styleText(props.icon)}`}
-              style={{color: props.color}}
-              title={props.tooltip}
-              />
-            )}
-
-          </div>
-
-          {/* Container holding item text and possibly a source */}
-          <div className={`${props.inline ? "d-inline mr-2" : ""} content-td pb-2 col`}>
-            <span className={`icon-item-text ${styleText(props.icon)}`}>
-              <Sanitized html={props.text} inline={!!props.inline} />
-            </span>
-            <Source source={props.source} sourceText={props.sourceText} />
-          </div>
-
-        </div>
-      )}
-    </Fragment>
-  );
-
+					{/* Container holding item text and possibly a source */}
+					<div
+						className={`${
+							props.inline ? 'd-inline mr-2' : ''
+						} content-td pb-2 col`}
+					>
+						<span className={`icon-item-text ${styleText(props.icon)}`}>
+							<Sanitized html={props.text} inline={!!props.inline} />
+						</span>
+						<Source source={props.source} sourceText={props.sourceText} />
+					</div>
+				</div>
+			)}
+		</Fragment>
+	)
 }
-export default BulletPointItem;
+export default BulletPointItem
 
 BulletPointItem.propTypes = {
-  id: PropTypes.number,
-  text: PropTypes.string,
-  icon: PropTypes.string,
-  indentation: PropTypes.number,
-  mode: PropTypes.number,
-  color: PropTypes.string,
-  tooltip: PropTypes.string,
-  setCheck: PropTypes.func,
-  checked: PropTypes.bool,
-  highlightStyle: PropTypes.number,
-  internal: PropTypes.number,
-  source: PropTypes.number,
-  sourceText: PropTypes.string,
-  inline: PropTypes.number
-};
+	id: PropTypes.number,
+	text: PropTypes.string,
+	icon: PropTypes.string,
+	indentation: PropTypes.number,
+	mode: PropTypes.number,
+	color: PropTypes.string,
+	tooltip: PropTypes.string,
+	setCheck: PropTypes.func,
+	checked: PropTypes.bool,
+	highlightStyle: PropTypes.number,
+	internal: PropTypes.number,
+	source: PropTypes.number,
+	sourceText: PropTypes.string,
+	inline: PropTypes.number
+}
