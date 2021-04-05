4ca2298081a3da019fbba7549505073dfbc00680
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-04-05 00:40:04 -0700

    added select icon to sub bullet point, items now change opacity on click. need to implement annotation next

--- client/src/pages/ContentPage/Card/BulletPoint.js
+++ client/src/pages/ContentPage/Card/BulletPoint.js
@@ -10,7 +10,11 @@ import { AiOutlinePlusCircle } from 'react-icons/ai'
 // Represents a single bullet point inside a card
 function BulletPoint(props) {
 	// set item selected state in create training mode (mode === 3)
-	const [selected, SetSelected] = useState(false)
+	const [selected, setSelected] = useState(false)
+
+	const handleSelect = e => {
+		setSelected(!selected)
+	}
 
 	// Don't show bullet points that are internal when we are viewing in public mode
 	return !props.internal || !props.publicMode ? (
@@ -20,84 +24,80 @@ function BulletPoint(props) {
     
     `}
 		>
-			{props.mode === 3 && (
-				<span className="training-select-icon">
-					<AiOutlinePlusCircle />
-				</span>
-			)}
-			<div style={{ display: 'inline-block' }}>
-				{/* If the bullet point is an item */}
-				{props.groupIndex === 1 && props.icon !== 'font' ? (
-					<BulletPointItem
-						id={props.id}
-						text={props.text}
-						icon={props.icon}
-						indentation={props.indentation}
-						mode={props.mode}
-						color={props.color}
-						tooltip={props.tooltip}
-						setCheck={(state, id) => props.setCheck(state, id)}
-						checked={props.checked}
-						highlightStyle={props.highlightStyle}
-						internal={props.internal}
-						source={props.source}
-						sourceText={props.sourceText}
-						inline={props.inline}
-					/>
-				) : null}
-				{/* If the bullet point is a text field */}
-				{props.groupIndex === 1 && props.icon === 'font' ? (
-					<BulletPointText
-						id={props.id}
-						text={props.text}
-						mode={props.mode}
-						indentation={props.indentation}
-						highlightStyle={props.highlightStyle}
-						internal={props.internal}
-						inline={props.inline}
-						source={props.source}
-						sourceText={props.sourceText}
-					/>
-				) : null}
-				{/* If the bullet point is a graphic */}
-				{props.groupIndex === 2 ? (
-					<BulletPointGraphic
-						text={props.text}
-						label={props.label}
-						url={props.url}
-						icon={props.icon}
-						indentation={props.indentation}
-						color={props.color}
-						tooltip={props.tooltip}
-						highlightStyle={props.highlightStyle}
-						internal={props.internal}
-						source={props.source}
-						sourceText={props.sourceText}
-						inline={props.inline}
-					/>
-				) : null}
-				{/* If the bullet point is a resource */}
-				{props.groupIndex === 3 ? (
-					<BulletPointResource
-						id={props.id}
-						text={props.text}
-						label={props.label}
-						url={props.url}
-						icon={props.icon}
-						created={props.created}
-						indentation={props.indentation}
-						mode={props.mode}
-						contentMode={props.contentMode}
-						handleTimestamp={m => props.handleTimestamp(m)}
-						color={props.color}
-						tooltip={props.tooltip}
-						reviewing={props.reviewing}
-						highlightStyle={props.highlightStyle}
-						internal={props.internal}
-						inline={props.inline}
-					/>
-				) : null}
-			</div>
+			{/* If the bullet point is an item */}
+			{props.groupIndex === 1 && props.icon !== 'font' ? (
+				<BulletPointItem
+					id={props.id}
+					text={props.text}
+					icon={props.icon}
+					indentation={props.indentation}
+					mode={props.mode}
+					color={props.color}
+					tooltip={props.tooltip}
+					setCheck={(state, id) => props.setCheck(state, id)}
+					checked={props.checked}
+					highlightStyle={props.highlightStyle}
+					internal={props.internal}
+					source={props.source}
+					sourceText={props.sourceText}
+					inline={props.inline}
+					handleSelect={handleSelect}
+					selected={selected}
+				/>
+			) : null}
+			{/* If the bullet point is a text field */}
+			{props.groupIndex === 1 && props.icon === 'font' ? (
+				<BulletPointText
+					id={props.id}
+					text={props.text}
+					mode={props.mode}
+					indentation={props.indentation}
+					highlightStyle={props.highlightStyle}
+					internal={props.internal}
+					inline={props.inline}
+					source={props.source}
+					sourceText={props.sourceText}
+				/>
+			) : null}
+			{/* If the bullet point is a graphic */}
+			{props.groupIndex === 2 ? (
+				<BulletPointGraphic
+					text={props.text}
+					label={props.label}
+					url={props.url}
+					icon={props.icon}
+					indentation={props.indentation}
+					mode={props.mode}
+					color={props.color}
+					tooltip={props.tooltip}
+					highlightStyle={props.highlightStyle}
+					internal={props.internal}
+					source={props.source}
+					sourceText={props.sourceText}
+					inline={props.inline}
+				/>
+			) : null}
+			{/* If the bullet point is a resource */}
+			{props.groupIndex === 3 ? (
+				<BulletPointResource
+					id={props.id}
+					text={props.text}
+					label={props.label}
+					url={props.url}
+					icon={props.icon}
+					created={props.created}
+					indentation={props.indentation}
+					mode={props.mode}
+					contentMode={props.contentMode}
+					handleTimestamp={m => props.handleTimestamp(m)}
+					color={props.color}
+					tooltip={props.tooltip}
+					reviewing={props.reviewing}
+					highlightStyle={props.highlightStyle}
+					internal={props.internal}
+					inline={props.inline}
+				/>
+			) : null}
 		</div>
 	) : null
 }
