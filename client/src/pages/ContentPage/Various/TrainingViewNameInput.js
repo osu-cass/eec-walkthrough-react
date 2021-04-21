eedfc53f608a645734faf64c6774b286d04449c3
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-04-20 20:50:39 -0700

    added emotionjs, handled input error for training path name and error for empty items

--- client/src/pages/ContentPage/Various/TrainingViewNameInput.js
+++ client/src/pages/ContentPage/Various/TrainingViewNameInput.js
@@ -2,14 +2,30 @@ import React, { useState } from 'react'
 import './TrainingViewNameInput.css'
 import { useSelector } from 'react-redux'
 import { getTrainingPageItems } from '../../../redux/selectors'
+import styled from '@emotion/styled'
+
+const ErrorContainer = styled.div`
+	margin-top: 0.3rem;
+	color: #dc3545;
+	font-size: 0.9rem;
+`
 
 function TrainingViewNameInput() {
 	const [inputValue, setInputValue] = useState('')
-
+	const [error, setError] = useState('')
 	const trainingPageItems = useSelector(getTrainingPageItems)
 
 	const handleFormSubmit = e => {
+		setError('')
 		e.preventDefault()
+		if (!inputValue) {
+			setError('Please enter a training path name')
+			return
+		}
+		if (!trainingPageItems.length) {
+			setError('Please select at least one training item')
+			return
+		}
 		console.log(trainingPageItems)
 	}
 
@@ -22,12 +38,14 @@ function TrainingViewNameInput() {
 					value={inputValue}
 					onChange={e => {
 						setInputValue(e.target.value)
+						setError('')
 					}}
 				/>
 				<button type="submit" className="btn btn-primary btn-save">
 					Save
 				</button>
 			</form>
+			{error && <ErrorContainer>{error}</ErrorContainer>}
 		</div>
 	)
 }
