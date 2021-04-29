57ef5c1dd2872fcffc5599beaf8889ae1d22c7e6
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-04-29 00:20:04 -0700

    formatted result from queries. ready for front end

---
+++ client/src/pages/TrainingPage/Section.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../ContentPage/Header/Header.css";
+
+
+
+function Section() {
+  return (
+    <div className="header-container header-approved my-3 py-3 text-dark-50 rounded shadow-sm border">
+      <h4 className="font-weight-bold col-auto align-self-center">This is a header</h4>
+    </div>
+  );
+}
+
+export default Section;
