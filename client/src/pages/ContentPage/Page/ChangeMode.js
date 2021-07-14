1fcbc14c2e69cd647fdb9f90edb942c8d246afb3
iKiwiz <kluong59@gmail.com>
2021-07-14 12:58:59 -0700

    changed ChangeMode to be dropdown instead of toggle

--- client/src/pages/ContentPage/Page/ChangeMode.js
+++ client/src/pages/ContentPage/Page/ChangeMode.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {Button} from "react-bootstrap";
 import PropTypes from "prop-types";
 import {setMode} from "../../../utilities/pageMode";
 
@@ -14,58 +13,105 @@ function ChangeMode(props) {
   if (props.role >= 3 && props.mode === 0) {
     return (
       <div className="text-center mx-2 my-auto d-print-none">
-        <Button size="sm" variant="secondary" onClick={() => toggleMode(1)}>
+        <button
+          className="btn btn-secondary btn-sm"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          id="dropdown-page-other"
+        >
           <i
             className="fas fa-eye text-white mr-2"
             style={{transform: "scale(1.5)"}}
           />
-          <span className="text-white">View Mode</span>
-        </Button>
+          <span className="text-white">View Mode </span>
+          <i
+            className="fas fa-chevron-down text-white"
+          />
+        </button>
+        <div className="dropdown-menu drop-down-z" aria-labelledby="dropdown-page-other">
+          <span className="dropdown-item" onClick={() => toggleMode(1)}>Switch to edit mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(2)}>Switch to move mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(3)}>Switch to create training mode</span>
+        </div>
       </div>
     );
   } else if (props.role >= 3 && props.mode === 1) {
     return (
       <div className="text-center mx-2 my-auto d-print-none">
-        <Button size="sm" variant="secondary" onClick={() => toggleMode(2)}>
-          <i
-            className="fas fa-wrench text-white mr-2"
-            style={{transform: "scale(1.5)"}}
-          />
-          <span className="text-white">Edit Mode</span>
-        </Button>
-      </div>
-    );
-  } else if (props.role >= 3 && props.mode === 2) {
-    return (
-      <div className="text-center mx-2 my-auto d-print-none">
-        <Button size="sm" variant="secondary" onClick={() => {
-          // call dispatch to reset redux storage before entering create training mode
-          toggleMode(3);
-        }}>
-          <i
-            className="fas fa-arrow-down text-white mr-2"
-            style={{transform: "scale(1.5)"}}
-          />
-          <span className="text-white">Move Mode</span>
-        </Button>
-      </div>
-    );
-  } else if (props.role >= 3 && props.mode === 3) {
-    return (
-      <div className="text-center mx-2 my-auto d-print-none">
-        <Button
-          size="sm"
-          variant="secondary"
-          onClick={() => {
-            toggleMode(0);
-          }}
+        <button
+          className="btn btn-secondary btn-sm"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          id="dropdown-page-other"
         >
           <i
             className="fas fa-wrench text-white mr-2"
             style={{transform: "scale(1.5)"}}
           />
-          <span className="text-white"> Create Training Mode</span>
-        </Button>
+          <span className="text-white">Edit Mode </span>
+          <i
+            className="fas fa-chevron-down text-white"
+          />
+        </button>
+        <div className="dropdown-menu drop-down-z" aria-labelledby="dropdown-page-other">
+          <span className="dropdown-item" onClick={() => toggleMode(0)}>Switch to view mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(2)}>Switch to move mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(3)}>Switch to create training mode</span>
+        </div>
+      </div>
+    );
+  } else if (props.role >= 3 && props.mode === 2) {
+    return (
+      <div className="text-center mx-2 my-auto d-print-none">
+        <button
+          className="btn btn-secondary btn-sm"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          id="dropdown-page-other"
+        >
+          <i
+            className="fas fa-arrow-down text-white mr-2"
+            style={{transform: "scale(1.5)"}}
+          />
+          <span className="text-white">Move Mode </span>
+          <i
+            className="fas fa-chevron-down text-white"
+          />
+        </button>
+        <div className="dropdown-menu drop-down-z" aria-labelledby="dropdown-page-other">
+          <span className="dropdown-item" onClick={() => toggleMode(0)}>Switch to view mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(1)}>Switch to edit mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(3)}>Switch to create training mode</span>
+        </div>
+      </div>
+    );
+  } else if (props.role >= 3 && props.mode === 3) {
+    return (
+      <div className="text-center mx-2 my-auto d-print-none">
+        <button
+          className="btn btn-secondary btn-sm"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          id="dropdown-page-other"
+        >
+          <i
+            className="fas fa-wrench text-white ml mr-2"
+            style={{transform: "scale(1.5)"}}
+          />
+          <span className="text-white">Create Training Mode </span>
+          <i
+            className="fas fa-chevron-down text-white"
+          />
+        </button>
+        <div className="dropdown-menu drop-down-z" aria-labelledby="dropdown-page-other">
+          <span className="dropdown-item" onClick={() => toggleMode(0)}>Switch to view mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(1)}>Switch to edit mode</span>
+          <span className="dropdown-item" onClick={() => toggleMode(2)}>Switch to move mode</span>
+        </div>
       </div>
     );
   } else {
